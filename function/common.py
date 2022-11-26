d1590cdfb34afb7b1bdf14f5ffcc8ad38be5e85d
togakushi <togakushi>
2022-11-27 04:34:14 +0900

    キーワードに「変換なし」を追加

--- function/common.py
+++ function/common.py
@@ -127,7 +127,7 @@ def argument_analysis(argument, command_option):
 
         if re.match(r"^ゲスト(なし|ナシ|無し|除外)$", keyword):
             command_option["guest_skip"] = False
-        if re.match(r"^修正(なし|ナシ|無し)$", keyword):
+        if re.match(r"^(修正|変換)(なし|ナシ|無し)$", keyword):
             command_option["name_replace"] = False
         if re.match(r"^(戦績)$", keyword):
             command_option["results"] = True
