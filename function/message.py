1c6b62c0c35a77e6625bcbd6fbe68e9edf4ace24
togakushi <togakushi>
2022-11-27 20:55:05 +0900

    ヘルプメッセージ整理

--- function/message.py
+++ function/message.py
@@ -6,13 +6,10 @@ def help(command):
     msg += f"\n\t{command} help                このメッセージ"
     msg += f"\n\t{command} results             成績出力"
     msg += f"\n\t{command} record              張り付け用集計済みデータ出力"
-    msg += f"\n\t{command} allrecord           集計済み全データ出力(名前ブレ修正なし)"
     msg += f"\n\t{command} graph               ポイント推移グラフを表示"
     msg += f"\n\t{command} member | userlist   登録されているメンバー"
-    msg += f"\n\t{command} add                 メンバーの追加"
-    msg += f"\n\t{command} del                 メンバーの削除"
-    msg += f"\n\t{command} load                メンバーリストの再読み込み"
-    msg += f"\n\t{command} save                メンバーリストの保存"
+    msg += f"\n\t{command} add | del           メンバーの追加/削除"
+    msg += f"\n\t{command} load | save         メンバーリストの再読み込み/保存"
     msg += f"```"
     return(msg)
 
