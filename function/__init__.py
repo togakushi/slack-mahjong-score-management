a857db6d47cb81f7ecd455a4c4ba02c6b3c4d31e
togakushi <togakushi>
2022-11-28 14:59:47 +0900

    分割

--- function/__init__.py
+++ function/__init__.py
@@ -1,4 +1,5 @@
 from function.common import *
+from function.translation import *
 from function.message import *
 from function.slack_api import *
 from function.score import *
--- function/common.py
+++ function/common.py
@@ -24,45 +24,6 @@ def configsave(config, configfile):
         config.write(f)
 
 
-def len_count(text): # 文字数
-    count = 0
-    for c in text:
-        if unicodedata.east_asian_width(c) in "FWA":
-            count += 2
-        else:
-            count += 1
-
-    return(count)
-
-
-def HAN2ZEN(text): # 全角変換(数字のみ)
-    ZEN = "".join(chr(0xff10 + i) for i in range(10))
-    HAN = "".join(chr(0x30 + i) for i in range(10))
-    trans_table = str.maketrans(HAN, ZEN)
-    return(text.translate(trans_table))
-
-
-def ZEN2HAN(text): # 半角変換(数字のみ)
-    ZEN = "".join(chr(0xff10 + i) for i in range(10))
-    HAN = "".join(chr(0x30 + i) for i in range(10))
-    trans_table = str.maketrans(ZEN, HAN)
-    return(text.translate(trans_table))
-
-
-def HIRA2KANA(text):
-    HIRA = "".join(chr(0x3041 + i) for i in range(86))
-    KANA = "".join(chr(0x30a1 + i) for i in range(86))
-    trans_table = str.maketrans(HIRA, KANA)
-    return(text.translate(trans_table)) 
-
-
-def KANA2HIRA(text):
-    HIRA = "".join(chr(0x3041 + i) for i in range(86))
-    KANA = "".join(chr(0x30a1 + i) for i in range(86))
-    trans_table = str.maketrans(KANA, HIRA)
-    return(text.translate(trans_table)) 
-
-
 def scope_coverage(target_days):
     startday = min(target_days)
     endday = max(target_days)
