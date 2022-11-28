2cffcf481c632d2c005a9c292a394b2b449a5317
togakushi <togakushi>
2022-11-28 20:30:57 +0900

    ゲスト無効オプションを追加

--- function/common.py
+++ function/common.py
@@ -109,6 +109,8 @@ def argument_analysis(argument, command_option):
         if re.match(r"^ゲスト(あり|アリ含む)$", keyword):
             command_option["guest_skip"] = True
             command_option["guest_skip2"] = True
+        if re.match(r"^ゲスト無効$", keyword):
+            command_option["unregistered_replace"] = False
         if re.match(r"^(修正|変換)(なし|ナシ|無し)$", keyword):
             command_option["playername_replace"] = False
         if re.match(r"^(戦績)$", keyword):
