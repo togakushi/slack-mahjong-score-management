57ceb2e77220a432fe1b6a76d350f1461e7affcf
togakushi <togakushi>
2022-11-30 13:03:43 +0900

    サブコマンドを分離

--- command/slash.py
+++ command/slash.py
@@ -4,9 +4,28 @@ from function import global_value as g
 
 
 commandname = g.config["slash"].get("commandname", "/goburei")
-results_alias = g.config["alias"].get("results", "成績")
-graph_alias = g.config["alias"].get("graph", "グラフ")
-record_alias = g.config["alias"].get("record", "記録")
+
+
+def subcommand_list(subcommand):
+    """
+    サブコマンドのリストを返す
+
+    Parameters
+    ----------
+    subcommand : str
+        サブコマンド名
+    
+    Returns
+    -------
+    commandlist : list
+        エイリアスを含めたサブコマンドのリスト
+    """
+
+    commandlist = g.config["alias"].get(subcommand, "")
+    commandlist = [subcommand] + [x for x in commandlist.split(",") if x]
+
+    return(commandlist)
+
 
 @g.app.command(commandname)
 def goburei_command(ack, body, client):
@@ -18,47 +37,47 @@ def goburei_command(ack, body, client):
         argument = body["text"].split()[1:]
 
         # 成績管理系コマンド
-        if subcom.lower() in (["results"] + results_alias.split(",")):
+        if subcom.lower() in subcommand_list("results"):
             command_option = f.command_option_initialization("results")
             g.logging.info(f"[subcommand({subcom})] {argument} {command_option}")
             c.results.slackpost(client, user_id, argument, command_option)
             return
 
-        if subcom.lower() in (["graph"] + graph_alias.split(",")):
+        if subcom.lower() in subcommand_list("graph"):
             command_option = f.command_option_initialization("graph")
             g.logging.info(f"[subcommand({subcom})] {argument} {command_option}")
             c.graph.slackpost(client, user_id, argument, command_option)
             return
 
-        if subcom.lower() in (["record"] + record_alias.split(",")):
+        if subcom.lower() in subcommand_list("record"):
             command_option = f.command_option_initialization("record")
             g.logging.info(f"[subcommand({subcom})] {argument} {command_option}")
-            title, msg = c.record.getdata(playername_replace = True, guest_skip = True)
+            title, msg = c.record.getdata(command_option)
             f.slack_api.post_upload(client, user_id, title, msg)
             return
 
         # メンバー管理系コマンド
-        if subcom.lower() in ("member", "userlist", "メンバー", "リスト"):
+        if subcom.lower() in subcommand_list("member"):
             title, msg = c.member.list()
             f.slack_api.post_text(client, user_id, title, msg)
             return
 
-        if subcom.lower() in ("add", "追加"):
+        if subcom.lower() in subcommand_list("add"):
             msg = c.member.Append(argument)
             f.slack_api.post_message(client, user_id, msg)
             return
 
-        if subcom.lower() in ("del", "削除"):
+        if subcom.lower() in subcommand_list("del"):
             msg = c.member.Remove(argument)
             f.slack_api.post_message(client, user_id, msg)
             return
 
-        if subcom.lower() in ("load"):
+        if subcom.lower() in subcommand_list("load"):
             g.player_list = f.options.configload(g.args.member)
             f.slack_api.post_message(client, user_id, f"メンバーリストを再読み込みしました。")
             return
 
-        if subcom.lower() in ("save"):
+        if subcom.lower() in subcommand_list("save"):
             f.options.configsave(g.player_list, g.args.member)
             f.slack_api.post_message(client, user_id, f"メンバーリストを保存しました。")
             return
