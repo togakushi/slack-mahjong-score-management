b5dbc1e05cc1f93bb449a85c4b07e3ab0f4694ec
togakushi <togakushi>
2023-10-10 03:01:19 +0900

    Homeタブ追加

--- slack-app.py
+++ slack-app.py
@@ -37,8 +37,137 @@ def handle_message_events():
 
 
 @g.app.event("app_home_opened")
-def handle_home_events():
-    pass
+def handle_home_events(client, event):
+    user_id = event["user"]
+
+    result = client.views_publish(
+        user_id = user_id,
+        view = {
+            "type": "home",
+            "blocks": [
+                {
+                    "type": "input",
+                    "block_id": "block_id-sday",
+                    "element": {
+                        "type": "datepicker",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select a date"
+                        },
+                        "action_id": "sday"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "検索開始日"
+                    },
+                },
+                {
+                    "type": "input",
+                    "block_id": "block_id-eday",
+                    "element": {
+                        "type": "datepicker",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select a date"
+                        },
+                        "action_id": "eday"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "検索終了日"
+                    }
+                },
+                {
+                    "type": "input",
+                    "block_id": "block_id-checkboxes",
+                    "element": {
+                        "type": "checkboxes",
+                        "options": [
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "ゲスト無効"
+                                },
+                                "value": "unregistered_replace"
+                            }
+                        ],
+                        "initial_options": [
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "ゲスト無効"
+                                },
+                                "value": "unregistered_replace"
+                            }
+                        ],
+                        "action_id": "checkboxes"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "検索オプション"
+                    }
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "御無礼サーチ"
+                            },
+                            "value": "gobrei_search",
+                            "action_id": "actionId-0"
+                        }
+                    ]
+                }
+            ]
+        }
+    )
+
+    g.logging.trace(result)
+
+@g.app.action("actionId-0")
+def handle_some_action(ack, body, client):
+    ack()
+    g.logging.trace(body)
+
+    command_option = f.configure.command_option_initialization("results")
+
+    user_id = body["user"]["id"]
+    sday = body["view"]["state"]["values"]["block_id-sday"]["sday"]["selected_date"]
+    eday = body["view"]["state"]["values"]["block_id-eday"]["eday"]["selected_date"]
+
+    if sday != None:
+        command_option["aggregation_range"] = []
+        command_option["aggregation_range"].append(sday.replace("-",""))
+    if eday != None:
+        command_option["aggregation_range"].append(eday.replace("-",""))
+
+    g.logging.info(command_option)
+    target_days, target_player, target_count, command_option = f.common.argument_analysis("", command_option)
+    starttime, endtime = f.common.scope_coverage(target_days)
+    if starttime and endtime:
+        msg = c.results.summary(starttime, endtime, target_player, target_count, command_option)
+
+    g.logging.info(body["view"]["state"]["values"]["block_id-checkboxes"]["checkboxes"])
+
+    client.views_publish(
+        user_id = user_id,
+        view = {
+            "type": "home",
+            "blocks": [
+                {
+                    "type": "section",
+                    "block_id": "PlainText",
+                    "text": {
+                        "type": "plain_text",
+                        "text": msg
+                    }
+                }
+            ]
+        }
+    )
 
 
 if __name__ == "__main__":
