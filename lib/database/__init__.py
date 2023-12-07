1eb33f1fdbb68b7e7f84daa12b0e5836ab161b6a
togakushi <togakushi>
2023-12-07 14:50:36 +0900

    突合コマンド仮実装

--- lib/database/__init__.py
+++ lib/database/__init__.py
@@ -1,2 +1,3 @@
 from lib.database.common import *
+from lib.database.comparison import *
 from lib.database.initialization import *
--- lib/database/comparison.py
+++ lib/database/comparison.py
@@ -6,6 +6,105 @@ import lib.command as c
 import lib.function as f
 from lib.function import global_value as g
 
+
+commandword = g.config["database"].get("commandword", "麻雀成績チェック")
+g.logging.info(f"[import] results {commandword}")
+
+
+# イベントAPI
+@g.app.message(re.compile(rf"^{commandword}"))
+def handle_results_evnts(client, context, body):
+    g.logging.trace(f"{body['event']}")
+    command = body["event"]["text"].split()[0]
+    argument = body["event"]["text"].split()[1:]
+    event_ts = body['event']['ts']
+
+    if not re.match(rf"^{commandword}$", command):
+        return
+
+    command_option = f.configure.command_option_initialization("record")
+    command_option["unregistered_replace"] = False # ゲスト無効
+    g.logging.info(f"[{command}] {command_option} {argument}")
+
+    slack_data = slack_search(command_option)
+    if slack_data == None:
+        return
+
+    resultdb = sqlite3.connect(g.database_file, detect_types = sqlite3.PARSE_DECLTYPES)
+    resultdb.row_factory = sqlite3.Row
+    cur = resultdb.cursor()
+
+    fts = list(slack_data.keys())[0]
+    db_data = databese_search(cur, fts.split(".")[0] + ".0")
+    if db_data == None:
+        return
+
+    # 突合処理
+    #resultdb = sqlite3.connect(g.database_file, detect_types = sqlite3.PARSE_DECLTYPES)
+    #resultdb.row_factory = sqlite3.Row
+    #cur = resultdb.cursor()
+
+    mismatch = 0
+    missing = 0
+    delete = 0
+
+    slack_data2 = []
+    for key in slack_data.keys():
+        skey1 = key
+        skey2 = key.split(".")[0] + ".0"
+        slack_data2.append(skey2)
+
+        flg = False
+        if skey1 in db_data.keys():
+            if slack_data[key] == db_data[skey1]:
+                continue
+            else:
+                flg = True
+                skey = skey1
+
+        if skey2 in db_data.keys():
+            if slack_data[key] == db_data[skey2]:
+                continue
+            else:
+                flg = True
+                skey = skey2
+
+
+        if flg:
+            mismatch += 1
+            #更新
+            print("[mismatch]:", skey)
+            print(" * [slack]:", slack_data[key])
+            print(" * [   db]:", db_data[skey])
+            db_update(cur, skey, slack_data[key])
+            continue
+
+        #追加
+        missing += 1
+        print("[missing ]:", key, slack_data[key])
+        db_insert(cur, key, slack_data[key])
+
+    for key in db_data.keys():
+        skey1 = key
+        skey2 = key.split(".")[0] + ".0"
+        if skey1 in slack_data.keys():
+            continue
+        if skey2 in slack_data2:
+            continue
+
+        print("[delete  ]:", key, db_data[key])
+        delete += 1
+        db_delete(cur, key)
+
+    print(f">>> mismatch:{mismatch}, missing:{missing}, delete:{delete}")
+    msg = f"不一致: {mismatch}件\n取りこぼし:{missing}件\n削除漏れ:{delete}件"
+    f.slack_api.post_message(client, context.channel_id, msg, event_ts)
+
+    resultdb.commit()
+    resultdb.close()
+
+
+
 def slack_search(command_option):
     """
     過去ログからスコアを検索して返す
@@ -65,11 +164,11 @@ def slack_search(command_option):
                     p2_name = c.NameReplace(msg[2], command_option, add_mark = False)
                     p3_name = c.NameReplace(msg[4], command_option, add_mark = False)
                     p4_name = c.NameReplace(msg[6], command_option, add_mark = False)
-                    g.logging.info("post data:[{} {} {}][{} {} {}][{} {} {}][{} {} {}]".format(
-                        "東家", p1_name, msg[1], "南家", p2_name, msg[3],
-                        "西家", p3_name, msg[5], "北家", p4_name, msg[7],
-                        )
-                    )
+                    #g.logging.info("post data:[{} {} {}][{} {} {}][{} {} {}][{} {} {}]".format(
+                    #    "東家", p1_name, msg[1], "南家", p2_name, msg[3],
+                    #    "西家", p3_name, msg[5], "北家", p4_name, msg[7],
+                    #    )
+                    #)
                     data[ts] = [p1_name, msg[1], p2_name, msg[3], p3_name, msg[5], p4_name, msg[7]]
 
     # slackのログに記録が1件もない場合は何もしない
@@ -78,13 +177,81 @@ def slack_search(command_option):
     else:
         return(data)
 
-    # ToDo:
-    # 見つかったログの最小 ts を起点にDBをselect
-    # ログのtsがDBから見つかるかチェック
-    # 見つかった→内容比較、update
-    # 見つからない→ミリ秒を削って再チェック
-    # それでも見つからない→取りこぼし、insert
 
-    # DBのtsとログのtsの比較
-    # DB側だけにあるts→ポストが削除された/管理から外された→delete
-    # ミリ秒を削るパターンも
+def databese_search(cur, first_ts = False):
+    if not first_ts:
+        return(None)
+
+    resultdb = sqlite3.connect(g.database_file, detect_types = sqlite3.PARSE_DECLTYPES)
+    resultdb.row_factory = sqlite3.Row
+
+    data ={}
+    rows = cur.execute(f"select * from result where ts > ?", (first_ts,))
+    for row in rows.fetchall():
+        ts = row["ts"]
+        data[ts] = []
+        data[ts].append(row["p1_name"])
+        data[ts].append(row["p1_str"])
+        data[ts].append(row["p2_name"])
+        data[ts].append(row["p2_str"])
+        data[ts].append(row["p3_name"])
+        data[ts].append(row["p3_str"])
+        data[ts].append(row["p4_name"])
+        data[ts].append(row["p4_str"])
+
+    return(data)
+
+
+def db_update(cur, ts, msg): # 突合処理専用
+    command_option = f.configure.command_option_initialization("results")
+    command_option["unregistered_replace"] = False # ゲスト無効
+
+    # ポイント計算
+    rpoint_data =[eval(msg[1]), eval(msg[3]), eval(msg[5]), eval(msg[7])]
+    deposit = g.config["mahjong"].getint("point", 250) * 4 - sum(rpoint_data)
+    array = {"p1": {}, "p2": {}, "p3": {}, "p4": {}}
+    for i1, i2 in ("p1",0),("p2",1),("p3",2),("p4",3):
+        array[i1]["name"] = c.member.NameReplace(msg[i2 * 2], command_option, False)
+        array[i1]["str"] = msg[i2 * 2 + 1]
+        array[i1]["rpoint"] = rpoint_data[i2]
+        array[i1]["rank"], array[i1]["point"] = f.CalculationPoint2(rpoint_data, rpoint_data[i2], i2)
+
+    cur.execute(g.sql_result_update, (
+        array["p1"]["name"], array["p1"]["str"], array["p1"]["rpoint"], array["p1"]["rank"], array["p1"]["point"],
+        array["p2"]["name"], array["p2"]["str"], array["p2"]["rpoint"], array["p2"]["rank"], array["p2"]["point"],
+        array["p3"]["name"], array["p3"]["str"], array["p3"]["rpoint"], array["p3"]["rank"], array["p3"]["point"],
+        array["p4"]["name"], array["p4"]["str"], array["p4"]["rpoint"], array["p4"]["rank"], array["p4"]["point"],
+        deposit,
+        ts
+        )
+    )
+
+
+def db_insert(cur, ts, msg): # 突合処理専用
+    command_option = f.configure.command_option_initialization("results")
+    command_option["unregistered_replace"] = False # ゲスト無効
+
+    # ポイント計算
+    rpoint_data =[eval(msg[1]), eval(msg[3]), eval(msg[5]), eval(msg[7])]
+    deposit = g.config["mahjong"].getint("point", 250) * 4 - sum(rpoint_data)
+    array = {"p1": {}, "p2": {}, "p3": {}, "p4": {}}
+    for i1, i2 in ("p1",0),("p2",1),("p3",2),("p4",3):
+        array[i1]["name"] = c.member.NameReplace(msg[i2 * 2], command_option, False)
+        array[i1]["str"] = msg[i2 * 2 + 1]
+        array[i1]["rpoint"] = rpoint_data[i2]
+        array[i1]["rank"], array[i1]["point"] = f.CalculationPoint2(rpoint_data, rpoint_data[i2], i2)
+
+    cur.execute(g.sql_result_insert, (
+        ts, datetime.fromtimestamp(float(ts)),
+        array["p1"]["name"], array["p1"]["str"], array["p1"]["rpoint"], array["p1"]["rank"], array["p1"]["point"],
+        array["p2"]["name"], array["p2"]["str"], array["p2"]["rpoint"], array["p2"]["rank"], array["p2"]["point"],
+        array["p3"]["name"], array["p3"]["str"], array["p3"]["rpoint"], array["p3"]["rank"], array["p3"]["point"],
+        array["p4"]["name"], array["p4"]["str"], array["p4"]["rpoint"], array["p4"]["rank"], array["p4"]["point"],
+        deposit,
+        g.config["mahjong"].get("rule_version", ""), ""
+        )
+    )
+
+
+def db_delete(cur, ts): # 突合処理専用
+    cur.execute(g.sql_result_delete, (ts,))
