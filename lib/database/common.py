b8d86f671b75af55d3e8f7423fbffe9194be0d0b
togakushi <togakushi>
2023-12-14 07:25:35 +0900

    未使用の関数を削除

--- lib/database/common.py
+++ lib/database/common.py
@@ -6,35 +6,6 @@ import lib.function as f
 from lib.function import global_value as g
 
 
-def select_table(cur, command_option):
-    ret = cur.execute(
-        "SELECT playtime, seat, player, rpoint, rank FROM 'gameresults';"
-    )
-
-    data = {}
-    count = 0
-
-    for row in ret.fetchall():
-        if not count in data:
-            data[count] = {}
-
-        data[count]["日付"] = row["playtime"]
-        data[count][g.wind[row["seat"]]] = {
-            "name": row["player"],
-            "rpoint": row["rpoint"],
-            "rank": row["rank"],
-            "point": f.score.CalculationPoint(row["rpoint"], row["rank"]),
-        }
-
-        if row["seat"] == 3:
-            count += 1
-    
-    if g.args.std:
-        print(data)
-
-    return(data)
-
-
 def ExsistRecord(ts):
     resultdb = sqlite3.connect(g.database_file, detect_types = sqlite3.PARSE_DECLTYPES)
     row = resultdb.execute("select ts from result where ts=?", (ts,))
