bca1985a214303133625353732aba34a17f63903
togakushi <togakushi>
2023-12-13 20:25:04 +0900

    view追加

--- lib/database/initialization.py
+++ lib/database/initialization.py
@@ -6,7 +6,8 @@ def initialization_resultdb():
     resultdb = sqlite3.connect(g.database_file, detect_types = sqlite3.PARSE_DECLTYPES)
 
     resultdb.execute(
-        """create table if not exists "member" (
+        """
+        create table if not exists "member" (
             "id"        INTEGER,
             "name"      TEXT NOT NULL UNIQUE,
             "slack_id"  TEXT,
@@ -14,19 +15,23 @@ def initialization_resultdb():
             "reward"    INTEGER DEFAULT 0,
             "abuse"     INTEGER DEFAULT 0,
             PRIMARY KEY("id" AUTOINCREMENT)
-        );"""
+        );
+        """
     )
 
     resultdb.execute(
-        """create table if not exists "alias" (
+        """
+        create table if not exists "alias" (
             "name"      TEXT,
             "member"    TEXT NOT NULL,
             PRIMARY KEY("name")
-        );"""
+        );
+        """
     )
 
     resultdb.execute(
-        """create table if not exists "result" (
+        """
+        create table if not exists "result" (
             "ts"            TEXT,
             "playtime"      TIMESTAMP UNIQUE,
             "p1_name"       TEXT NOT NULL,
@@ -53,7 +58,74 @@ def initialization_resultdb():
             "rule_version"  TEXT,
             "comment"       TEXT,
             PRIMARY KEY("ts")
-        );"""
+        );
+        """
+    )
+
+    resultdb.execute(
+        """
+        create view if not exists individual as
+            select
+                datetime(playtime) as playtime,
+                p1_name as name,
+                p1_rpoint as rpoint,
+                p1_rank as rank,
+                p1_point as point,
+                substr(
+                    case when
+                        time(playtime) between "00:00:00" and "11:59:59"
+                            then date(playtime, "-1 days")
+                            else date(playtime)
+                    end, 1, 7
+                ) as collection
+            from
+                result
+            union select 
+                datetime(playtime),
+                p2_name,
+                p2_rpoint,
+                p2_rank,
+                p2_point,
+                substr(
+                    case when
+                        time(playtime) between "00:00:00" and "11:59:59"
+                            then date(playtime, "-1 days")
+                            else date(playtime)
+                    end, 1, 7
+                )
+            from
+                result
+            union select
+                datetime(playtime),
+                p3_name,
+                p3_rpoint,
+                p3_rank,
+                p3_point,
+                substr(
+                    case when
+                        time(playtime) between "00:00:00" and "11:59:59"
+                            then date(playtime, "-1 days")
+                            else date(playtime)
+                    end, 1, 7
+                )
+            from
+                result
+            union select
+                datetime(playtime),
+                p4_name,
+                p4_rpoint,
+                p4_rank,
+                p4_point,
+                substr(
+                    case when
+                        time(playtime) between "00:00:00" and "11:59:59"
+                            then date(playtime, "-1 days")
+                            else date(playtime)
+                    end, 1, 7
+                )
+            from
+                result
+        """
     )
 
     # ゲスト設定チェック
@@ -62,12 +134,12 @@ def initialization_resultdb():
 
     if len(data) == 0:
         g.logging.info(f"ゲスト設定: {g.guest_name}")
-        sql = "insert into member (id, name) values (?, ?)"
-        resultdb.execute(sql, [0, g.guest_name])
+        sql = "insert into member (id, name) values (0, ?)"
+        resultdb.execute(sql, (g.guest_name,))
     elif data[0][1] != g.guest_name:
         g.logging.info(f"ゲスト修正: {data[0][1]} -> {g.guest_name}")
         sql = "update member set name=? where id=0"
-        resultdb.execute(sql, [g.guest_name])
+        resultdb.execute(sql, (g.guest_name,))
 
     resultdb.commit()
     resultdb.close()
