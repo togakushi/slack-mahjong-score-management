7dab1d06ca8b9f49e265afd67f9fe6e83943a1e8
togakushi <togakushi>
2023-12-28 13:21:22 +0900

    ゲーム数のカウントクエリを追加

--- lib/command/results/summary.py
+++ lib/command/results/summary.py
@@ -6,7 +6,7 @@ import lib.function as f
 from lib.function import global_value as g
 
 
-def __select_game__(argument, command_option):
+def select_game(argument, command_option):
     target_days, target_player, target_count, command_option = f.common.argument_analysis(argument, command_option)
     starttime, endtime = f.common.scope_coverage(target_days)
 
@@ -83,7 +83,7 @@ def __select_game__(argument, command_option):
     g.logging.trace(f"sql: {sql}")
     g.logging.trace(f"placeholder: {placeholder}")
 
-    ret = {
+    return {
         "target_days": target_days,
         "target_player": target_player,
         "target_count": target_count,
@@ -93,7 +93,74 @@ def __select_game__(argument, command_option):
         "placeholder": placeholder,
     }
 
-    return(ret)
+def count_game(argument, command_option):
+    target_days, target_player, target_count, command_option = f.common.argument_analysis(argument, command_option)
+    starttime, endtime = f.common.scope_coverage(target_days)
+
+    g.logging.info(f"date range: {starttime} {endtime}  target_count: {target_count}")
+    g.logging.info(f"target_player: {target_player}")
+    g.logging.info(f"command_option: {command_option}")
+
+    sql = """
+        --[recent] select * from (
+        select
+            count() as count
+        from (
+            select
+                playtime
+            from
+                individual_results
+            where
+                rule_version = ?
+                and playtime between ? and ? -- 検索範囲
+                --[guest_not_skip] and playtime not in (select playtime from individual_results group by playtime having sum(guest) >= 2) -- ゲストあり
+                --[target_player] and name in (<<target_player>>) -- 対象プレイヤー
+            group by
+                playtime
+            order by
+                playtime desc
+            --[recent] limit ?
+        )
+        --[recent] )
+    """
+
+    placeholder = [g.rule_version, starttime, endtime]
+
+    if command_option["unregistered_replace"]:
+        sql = sql.replace("--[unregistered_replace] ", "")
+        if command_option["guest_skip"]:
+            sql = sql.replace("--[guest_not_skip] ", "")
+        else:
+            sql = sql.replace("--[guest_skip] ", "")
+    else:
+        sql = sql.replace("--[unregistered_not_replace] ", "")
+    if target_player:
+        sql = sql.replace("--[target_player] ", "")
+        p = []
+        for i in target_player:
+            p.append("?")
+            placeholder.append(i)
+        sql = sql.replace("<<target_player>>", ",".join([i for i in p]))
+
+    if target_count != 0:
+        sql = sql.replace("and playtime between", "-- and playtime between")
+        sql = sql.replace("--[recent] ", "")
+        placeholder.pop(placeholder.index(starttime))
+        placeholder.pop(placeholder.index(endtime))
+        placeholder += [target_count]
+
+    g.logging.trace(f"sql: {sql}")
+    g.logging.trace(f"placeholder: {placeholder}")
+
+    return {
+        "target_days": target_days,
+        "target_player": target_player,
+        "target_count": target_count,
+        "starttime": starttime,
+        "endtime": endtime,
+        "sql": sql,
+        "placeholder": placeholder,
+    }
 
 
 def summary(argument, command_option):
@@ -120,9 +187,14 @@ def summary(argument, command_option):
         メモ内容
     """
 
-    ret = __select_game__(argument, command_option)
     resultdb = sqlite3.connect(g.database_file, detect_types = sqlite3.PARSE_DECLTYPES)
     resultdb.row_factory = sqlite3.Row
+
+    ret = count_game(argument, command_option)
+    rows = resultdb.execute(ret["sql"], ret["placeholder"])
+    gamecount = rows.fetchone()[0]
+
+    ret = select_game(argument, command_option)
     rows = resultdb.execute(ret["sql"], ret["placeholder"])
 
     # ---
@@ -146,6 +218,8 @@ def summary(argument, command_option):
     msg1 = ""
     msg2 = "*【成績サマリ】*\n"
     msg3 = ""
+    first_game = min([results[name]["first_game"] for name in results.keys()])
+    last_game = max([results[name]["last_game"] for name in results.keys()])
 
     # --- 情報ヘッダ
     if ret["target_count"] == 0: # 直近指定がない場合は検索範囲を付ける
@@ -153,23 +227,18 @@ def summary(argument, command_option):
             ret["starttime"].strftime('%Y/%m/%d %H:%M'), ret["endtime"].strftime('%Y/%m/%d %H:%M'),
         )
     msg2 += "\t最初のゲーム：{}\n\t最後のゲーム：{}\n".format(
-        min([results[name]["first_game"] for name in results.keys()]).replace("-", "/"),
-        max([results[name]["last_game"] for name in results.keys()]).replace("-", "/"),
+        first_game.replace("-", "/"), last_game.replace("-", "/"),
     )
     if ret["target_player"]:
-        msg2 += "\t総ゲーム数：{} 回".format(
-            int(sum([results[name]["count"] for name in results.keys()])),
-        )
+        msg2 += f"\t総ゲーム数：{gamecount} 回"
     else:
-        msg2 += "\tゲーム数：{} 回".format(
-            int(sum([results[name]["count"] for name in results.keys()]) / 4),
-        )
+        msg2 += f"\tゲーム数：{gamecount} 回"
 
     if g.config["mahjong"].getboolean("ignore_flying", False):
         msg2 += "\n"
     else:
         msg2 += " / トバされた人（延べ）： {} 人\n".format(
-            sum([results[name]["flying"] for name in results.keys()])
+            sum([results[name]["flying"] for name in results.keys()]),
         )
     msg2 += f.remarks(command_option)
 
@@ -220,8 +289,10 @@ def summary(argument, command_option):
 
         # --- メモ表示
         rows = resultdb.execute(
-            "select * from remarks where thread_ts between ? and ? order by thread_ts,event_ts",
-            (ret["starttime"].timestamp(), ret["endtime"].timestamp())
+            "select * from remarks where thread_ts between ? and ? order by thread_ts,event_ts", (
+                datetime.fromisoformat(first_game).timestamp(),
+                datetime.fromisoformat(last_game).timestamp(),
+            )
         )
         for row in rows.fetchall():
             name = c.NameReplace(row["name"], command_option)
