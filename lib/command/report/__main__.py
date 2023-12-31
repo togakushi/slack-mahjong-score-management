f3e7dbe464c88bdfc891d7c19fffbcfd2120bcf6
togakushi <togakushi>
2023-12-31 10:23:34 +0900

    個人成績レポート追加

--- lib/command/report/__main__.py
+++ lib/command/report/__main__.py
@@ -27,8 +27,8 @@ def slackpost(client, channel, argument, command_option):
     g.logging.info(f"arg: {argument}")
     g.logging.info(f"opt: {command_option}")
 
-    #report_file_path = monthly.plot(argument, command_option)
-    #f.slack_api.post_fileupload(client, channel, "月別ゲーム統計", report_file_path)
+    report_file_path = monthly.plot(argument, command_option)
+    f.slack_api.post_fileupload(client, channel, "月別ゲーム統計", report_file_path)
 
     report_file_path = personal.plot(argument, command_option)
     f.slack_api.post_fileupload(client, channel, "個人成績", report_file_path)
