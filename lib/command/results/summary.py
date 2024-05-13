055e0817354a1bb244c16805af9c2c07f55763d9
togakushi <togakushi>
2024-05-14 08:45:54 +0900

    集計方法修正

--- lib/command/results/summary.py
+++ lib/command/results/summary.py
@@ -33,32 +33,24 @@ def aggregation(argument, command_option):
     """
 
     ### データ収集 ###
-    resultdb = sqlite3.connect(g.database_file, detect_types = sqlite3.PARSE_DECLTYPES)
-    resultdb.row_factory = sqlite3.Row
-    cur = resultdb.cursor()
-
-    total_game_count = d.common.game_count(argument, command_option, cur)
-    results, name_list, prams = query.select_game(argument, command_option, cur)
+    params = d.common.placeholder_params(argument, command_option)
+    total_game_count, first_game, last_game = d.aggregate.game_count(argument, command_option)
+    summary_data = d.aggregate.game_summary(argument, command_option)
 
     ### 表示 ###
-    if len(results) == 0: # 結果が0件のとき
+    if total_game_count == 0: # 結果が0件のとき
         return(None, f.message.no_hits(argument, command_option), None)
 
-    msg1 = ""
-    msg2 = "*【成績サマリ】*\n"
-    msg3 = ""
-    first_game = min([results[name]["first_game"] for name in results.keys()])
-    last_game = max([results[name]["last_game"] for name in results.keys()])
-
     # --- 情報ヘッダ
-    if prams["target_count"] == 0: # 直近指定がない場合は検索範囲を付ける
+    msg2 = "*【成績サマリ】*\n"
+    if params["target_count"] == 0: # 直近指定がない場合は検索範囲を付ける
         msg2 += "\t検索範囲：{} ～ {}\n".format(
-            prams["starttime"].strftime('%Y/%m/%d %H:%M'), prams["endtime"].strftime('%Y/%m/%d %H:%M'),
+            params["starttime"].strftime('%Y/%m/%d %H:%M'), params["endtime"].strftime('%Y/%m/%d %H:%M'),
         )
     msg2 += "\t最初のゲーム：{}\n\t最後のゲーム：{}\n".format(
-        first_game.replace("-", "/"), last_game.replace("-", "/"),
+        first_game.strftime("%Y/%m/%d %H:%M:%S"), last_game.strftime("%Y/%m/%d %H:%M:%S"),
     )
-    if prams["player_name"]:
+    if params["player_name"]:
         msg2 += f"\t総ゲーム数：{total_game_count} 回"
     else:
         msg2 += f"\tゲーム数：{total_game_count} 回"
@@ -67,73 +59,66 @@ def aggregation(argument, command_option):
         msg2 += "\n"
     else:
         msg2 += " / トバされた人（延べ）： {} 人\n".format(
-            sum([results[name]["flying"] for name in results.keys()]),
+            summary_data["flying"].sum(),
         )
     msg2 += f.message.remarks(command_option)
 
     # --- 集計結果
-    padding = c.member.CountPadding(list(set(name_list)))
+    msg3 = ""
+    padding = c.member.CountPadding(list(summary_data["name"].unique()))
     if command_option["score_comparisons"]: # 差分表示
-        header = "## {} {}： 累積    / 点差 ##\n".format(
+        msg1 = "## {} {}： 累積    / 点差 ##\n".format(
             "名前", " " * (padding - f.common.len_count("名前") - 4),
         )
-        previous_point = None
-        for name in results.keys():
-            pname = c.member.NameReplace(name, command_option, add_mark = True)
-            if previous_point == None:
-                msg1 += "{} {}： {:>+6.1f} / *****\n".format(
-                    pname, " " * (padding - f.common.len_count(pname)),
-                    results[name]["pt_total"],
-                ).replace("-", "▲").replace("*", "-")
-            else:
-                msg1 += "{} {}： {:>+6.1f} / {:>5.1f}\n".format(
-                    pname, " " * (padding - f.common.len_count(pname)),
-                    results[name]["pt_total"],
-                    previous_point - results[name]["pt_total"],
-                ).replace("-", "▲")
-            previous_point = results[name]["pt_total"]
+        for _, row in summary_data.iterrows():
+            msg1 += "{} {}： {:>+6.1f} / {:>5.1f}\n".format(
+                row["name"], " " * (padding - f.common.len_count(row["name"])),
+                row["pt_total"], row["pt_diff"],
+            ).replace("-", "▲").replace("nan", "****")
     else: # 通常表示
-        header = "## {} {} : 累積 (平均) / 順位分布 (平均)".format(
-            "名前", " " * (padding - f.common.len_count("名前") - 4),
-        )
-        if g.config["mahjong"].getboolean("ignore_flying", False):
-            header += " ##\n"
+        if g.config["mahjong"].getboolean("ignore_flying", False): # トビカウントなし
+            msg1 = "# {} {} :  累積   (平均)  / 順位分布 (平均) #\n".format(
+                "名前", " " * (padding - f.common.len_count("名前") - 3))
+            for _, row in summary_data.iterrows():
+                msg1 += "{} {}： {:>+6.1f} ({:>+5.1f}) / {}*{}*{}*{} ({:1.2f})\n".format(
+                    row["name"], " " * (padding - f.common.len_count(row["name"])),
+                    row["pt_total"], row["pt_avg"],
+                    row["1st"], row["2nd"], row["3rd"], row["4th"],
+                    row["rank_avg"],
+                ).replace("-", "▲").replace("*", "-")
         else:
-            header +=" / トビ ##\n"
-        for name in results.keys():
-            pname = c.member.NameReplace(name, command_option, add_mark = True)
-            msg1 += "{} {}： {:>+6.1f} ({:>+5.1f})".format(
-                pname, " " * (padding - f.common.len_count(pname)),
-                results[name]["pt_total"], results[name]["pt_avg"],
-            ).replace("-", "▲")
-            msg1 += " / {}-{}-{}-{} ({:1.2f})".format(
-                results[name]["1st"], results[name]["2nd"],
-                results[name]["3rd"], results[name]["4th"],
-                results[name]["rank_avg"],
-            )
-            if g.config["mahjong"].getboolean("ignore_flying", False):
-                msg1 += "\n"
-            else:
-                msg1 += f" / {results[name]['flying']}\n"
+            msg1 = "# {} {} :  累積   (平均)  / 順位分布 (平均) / トビ #\n".format(
+                "名前", " " * (padding - f.common.len_count("名前") - 3))
+            for index, row in summary_data.iterrows():
+                msg1 += "{} {}： {:>+6.1f} ({:>+5.1f}) / {}*{}*{}*{} ({:1.2f}) / {}\n".format(
+                    row["name"], " " * (padding - f.common.len_count(row["name"])),
+                    row["pt_total"], row["pt_avg"],
+                    row["1st"], row["2nd"], row["3rd"], row["4th"],
+                    row["rank_avg"], row["flying"],
+                ).replace("-", "▲").replace("*", "-")
 
         # --- メモ表示
+        resultdb = sqlite3.connect(g.database_file, detect_types = sqlite3.PARSE_DECLTYPES)
+        resultdb.row_factory = sqlite3.Row
         rows = resultdb.execute(
             "select * from remarks where thread_ts between ? and ? order by thread_ts,event_ts", (
-                datetime.fromisoformat(first_game).timestamp(),
-                max([results[i]["max_ts"] for i in results.keys()]),
+                first_game.timestamp(),
+                last_game.timestamp(),
             )
         )
         for row in rows.fetchall():
             g.logging.trace(dict(row)) # type: ignore
             name = c.member.NameReplace(row["name"], command_option, add_mark = True)
-            if name in name_list:
+            if name in list(summary_data["name"].unique()):
                 msg3 += "\t{}： {} （{}）\n".format(
                     datetime.fromtimestamp(float(row["thread_ts"])).strftime('%Y/%m/%d %H:%M:%S'),
                     row["matter"],
                     name,
                 )
 
+        resultdb.close()
+
     if msg3:
         msg3 = "*【メモ】*\n" + msg3
 
-    return(header + msg1, msg2, msg3)
+    return(msg1, msg2, msg3)
