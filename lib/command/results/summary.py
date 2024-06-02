83fd1f6865fb8dc98f0052defca80ab724cb8148
togakushi <togakushi>
2024-06-02 11:06:46 +0900

    集計方法変更

--- lib/command/results/summary.py
+++ lib/command/results/summary.py
@@ -28,8 +28,23 @@ def aggregation(argument, command_option):
     ### データ収集 ###
     params = f.configure.get_parameters(argument, command_option)
     total_game_count, first_game, last_game = d.aggregate.game_count(argument, command_option)
-    summary_df = d.aggregate.game_summary(argument, command_option)
-    game_df = d.aggregate.game_details(argument, command_option)
+    df_summary = d.aggregate.game_summary(argument, command_option)
+    df_game = d.aggregate.game_details(argument, command_option)
+    df_grandslam = df_game.query("grandslam != ''")
+
+    # ヘッダ更新 ToDo: SQLのカラム名を変える
+    df_summary.rename(
+        columns = {
+            "count": "ゲーム数", "pt_total": "累積ポイント", "pt_avg": "平均ポイント",
+            "1st": "1位", "2nd": "2位", "3rd": "3位", "4th": "4位", "rank_distr": "順位分布",
+            "rank_avg": "平均順位", "flying": "トビ", "pt_diff": "差分",
+        }, inplace = True)
+    df_summary["プレイヤー名"] = df_summary["表示名"].apply(lambda x: x.strip())
+    df_grandslam.rename(
+        columns = {
+            "プレイヤー名": "name", "grandslam": "和了役", "playtime": "日時",
+        }, inplace = True)
+    df_grandslam["プレイヤー名"] = df_grandslam["表示名"].apply(lambda x: x.strip())
 
     ### 表示 ###
     # --- 情報ヘッダ
@@ -47,62 +62,88 @@ def aggregation(argument, command_option):
             msg2 += "\n"
         else:
             msg2 += " / トバされた人（延べ）： {} 人\n".format(
-                summary_df["flying"].sum(),
+                df_summary["トビ"].sum(),
             )
         msg2 += "\t" + f.message.remarks(command_option)
     else: # 結果が0件のとき
+        msg2 += f"\tゲーム数：{total_game_count} 回"
         msg2 += "\t" + f.message.remarks(command_option)
-        return(msg2, f.message.no_hits(argument, command_option))
+        return(msg2, None, df_summary, df_grandslam)
 
     # --- 集計結果
-    padding = c.member.CountPadding(list(summary_df["表示名"].unique())) -7
+    padding = c.member.CountPadding(list(df_summary["表示名"].unique())) -7
+    msg = {}
     message = ""
+    msg_memo = ""
 
-    if command_option["score_comparisons"]: # 差分表示
-        header = f"\n```\n# 名前{' ' * padding} ： 累積    / 点差 #\n"
-        for _, v in summary_df.iterrows():
-            message += "{}： {:>+6.1f} / {:>5.1f}\n".format(
-                v["表示名"], v["pt_total"], v["pt_diff"],
-            ).replace("-", "▲")
-    else: # 通常表示
+    if not command_option["score_comparisons"]: # 通常表示
         if g.config["mahjong"].getboolean("ignore_flying", False): # トビカウントなし
             header = f"\n```\n# 名前{' ' * padding} ：  累積   (平均) / 順位分布 (平均) #\n"
-            for i, v in summary_df.iterrows():
+            filter_list = [
+                "プレイヤー名", "ゲーム数", "累積ポイント", "平均ポイント",
+                "1位", "2位", "3位", "4位", "平均順位",
+            ]
+            for i, v in df_summary.iterrows():
                 message += "{}： {} ({}) / {} ({:1.2f})\n".format(
                     v["表示名"],
-                    str(f"{v['pt_total']:>+6.1f}").replace("-", "▲"),
-                    str(f"{v['pt_avg']:>+5.1f}").replace("-", "▲"),
-                    v["rank_distr"], v["rank_avg"],
+                    str(f"{v['累積ポイント']:>+6.1f}").replace("-", "▲"),
+                    str(f"{v['平均ポイント']:>+5.1f}").replace("-", "▲"),
+                    v["順位分布"], v["平均順位"],
                 )
         else: # トビカウントあり
             header = f"\n```\n# 名前{' ' * padding} ：  累積   (平均) / 順位分布 (平均) / トビ #\n"
-            for i, v in summary_df.iterrows():
+            filter_list = [
+                "プレイヤー名", "ゲーム数", "累積ポイント", "平均ポイント",
+                "1位", "2位", "3位", "4位", "平均順位", "トビ",
+            ]
+            for i, v in df_summary.iterrows():
                 message += "{}： {} ({}) / {} ({:1.2f}) / {}\n".format(
                     v["表示名"],
-                    str(f"{v['pt_total']:>+6.1f}").replace("-", "▲"),
-                    str(f"{v['pt_avg']:>+5.1f}").replace("-", "▲"),
-                    v["rank_distr"], v["rank_avg"], v["flying"],
+                    str(f"{v['累積ポイント']:>+6.1f}").replace("-", "▲"),
+                    str(f"{v['平均ポイント']:>+5.1f}").replace("-", "▲"),
+                    v["順位分布"], v["平均順位"], v["トビ"],
                 )
 
-        # メッセージをstep行単位のブロックに分割
-        step = 50
-        msg = {}
-        for count in range(int(len(message.splitlines()) / step) + 1):
-            msg[count] = "\n".join(message.splitlines()[count * step:(count + 1) * step])
-
-        # 最終ブロックがstepの半分以下なら直前のブロックにまとめる
-        if count >= 1 and step / 2 > len(msg[count].splitlines()):
-            msg[count - 1] += "\n" + msg.pop(count)
-
-        # ヘッダ+コードブロック追加
-        for i in msg.keys():
-            msg[i] = header + msg[i] + "\n```\n"
-
         # --- メモ表示
-        grandslam_df = game_df.query("grandslam != ''")
-        if len(grandslam_df) != 0:
-            msg["メモ"] = "*【メモ】*\n"
-            for _, v in grandslam_df.iterrows():
-                msg["メモ"] += f"\t{v['playtime']} {v['表示名']} {v['grandslam']}\n"
+        if len(df_grandslam) != 0:
+            msg_memo = "*【メモ】*\n"
+            for _, v in df_grandslam.iterrows():
+                msg_memo += "\t{} ： {} （{}）\n".format(
+                    v["日時"].replace("-", "/"),
+                    v["和了役"],
+                    v["プレイヤー名"],
+                )
+    else: # 差分表示
+        df_grandslam = df_grandslam[:0]
+        header = f"\n```\n# 名前{' ' * padding} ： 累積    / 点差 #\n"
+        filter_list = ["プレイヤー名", "ゲーム数", "累積ポイント", "点差"]
+        for _, v in df_summary.iterrows():
+            message += "{}： {} / {}\n".format(
+                v["表示名"],
+                str(f"{v['累積ポイント']:>+6.1f}").replace("-", "▲"),
+                f"{v['差分']:>5.1f}" if type(v["差分"]) == float else v["差分"],
+            )
 
-    return(msg2, msg)
+    # --- メッセージ整形
+    # メッセージをstep行単位のブロックに分割
+    step = 50
+    for count in range(int(len(message.splitlines()) / step) + 1):
+        msg[count] = "\n".join(message.splitlines()[count * step:(count + 1) * step])
+
+    # 最終ブロックがstepの半分以下なら直前のブロックにまとめる
+    if count >= 1 and step / 2 > len(msg[count].splitlines()):
+        msg[count - 1] += "\n" + msg.pop(count)
+
+    # ヘッダ+コードブロック追加
+    for i in msg.keys():
+        msg[i] = header + msg[i] + "\n```\n"
+
+    # メモ追加
+    if msg_memo:
+        msg["メモ"] = msg_memo
+
+    # --- ファイル出力用データ整形
+    df_summary = df_summary.filter(items = filter_list)
+    df_grandslam = df_grandslam.filter(items = ["日時", "和了役", "プレイヤー名"])
+
+    return(msg2, msg, df_summary, df_grandslam)
--- lib/command/results/versus.py
+++ lib/command/results/versus.py
@@ -1,5 +1,7 @@
 import textwrap
 
+import pandas as pd
+
 import lib.command as c
 import lib.function as f
 import lib.database as d
@@ -25,15 +27,20 @@ def aggregation(argument, command_option):
 
     msg2 : dict
         slackにpostするデータ(スレッドに返す)
+
+    df_data : DataFrame
+        ファイル出力用データフレーム
+    df_vs
     """
 
     # 検索動作を合わせる
     command_option["guest_skip"] = command_option["guest_skip2"]
 
     # --- データ収集
-    param = d.common.placeholder_params(argument, command_option)
-    df = d.aggregate.versus_matrix(argument, command_option)
+    param = f.configure.get_parameters(argument, command_option)
+    df_vs = d.aggregate.versus_matrix(argument, command_option)
     df_game = d.aggregate.game_details(argument, command_option)
+    df_data = pd.DataFrame(columns = df_game.columns) # ファイル出力用
 
     # --- ヘッダ情報
     my_name = param["player_name"]
@@ -48,29 +55,28 @@ def aggregation(argument, command_option):
         *【直接対戦結果】*
         \tプレイヤー名：{c.member.NameReplace(my_name, command_option, add_mark = True)}
         \t対戦相手：{vs}
-        \t検索範囲：{param["starttime"].strftime("%Y/%m/%d %H:%M")} ～ {param["endtime"].strftime("%Y/%m/%d %H:%M")}
+        \t検索範囲：{param["starttime_hms"]} ～ {param["endtime_hms"]}
         \t{f.message.remarks(command_option)}
     """).strip() + "\n"
 
     # --- 表示内容
-    msg2 = {}
-    if len(df) == 0: # 検索結果なし
-        msg2[""] = "対戦記録が見つかりません。\n"
+    tmp_msg = {}
+    if len(df_vs) == 0: # 検索結果なし
+        tmp_msg[""] = "対戦記録が見つかりません。\n"
 
     for vs_name in param["competition_list"].values():
-        msg2[vs_name] = {}
+        tmp_msg[vs_name] = {}
         if vs_name in vs_list:
-            data = df.query("vs_name == @vs_name")
-
+            data = df_vs.query("vs_name == @vs_name")
             if len(data) == 0:
-                msg2[vs_name]["info"] = "【{} vs {}】\n\t対戦記録はありません。\n".format(
+                tmp_msg[vs_name]["info"] = "【{} vs {}】\n\t対戦記録はありません。\n".format(
                     c.member.NameReplace(my_name, command_option, add_mark = True),
                     c.member.NameReplace(vs_name, command_option, add_mark = True),
                 )
                 continue
 
             r = data.to_dict(orient = "records")[0]
-            msg2[vs_name]["info"] = textwrap.dedent(f"""
+            tmp_msg[vs_name]["info"] = textwrap.dedent(f"""
                 【{r["my_表示名"].strip()} vs {r["vs_表示名"].strip()}】
                 \t対戦数： {r["game"]} 戦 {r["win"]} 勝 {r["lose"]} 敗 ({r["win%"]:.2f}%)
                 \t平均素点差： {(r["my_rpoint_avg"]-r["vs_rpoint_avg"]) * 100:+.0f} 点
@@ -92,12 +98,13 @@ def aggregation(argument, command_option):
                     if playtime in my_playtime and playtime in vs_playtime:
                         current_game = df_game.query("playtime == @playtime")
                         guest_count = current_game["guest"].sum()
+                        df_data = current_game if df_data.empty else pd.concat([df_data, current_game])
                         if command_option["verbose"]: # 詳細表示
                             s1 = current_game.query("seat == 1").to_dict(orient = "records")[0]
                             s2 = current_game.query("seat == 2").to_dict(orient = "records")[0]
                             s3 = current_game.query("seat == 3").to_dict(orient = "records")[0]
                             s4 = current_game.query("seat == 4").to_dict(orient = "records")[0]
-                            msg2[vs_name][count] = textwrap.dedent(f"""
+                            tmp_msg[vs_name][count] = textwrap.dedent(f"""
                                 {"*【戦績】*" if count == 0 else ""}
                                 {playtime.replace("-", "/")} {"(2ゲスト戦)" if guest_count >= 2 else ""}
                                 \t東家：{s1["表示名"]} {s1["rank"]}位 {s1["rpoint"] * 100:>7} 点 ({s1["point"]:>+5.1f}pt) {s1["grandslam"]}
@@ -108,17 +115,64 @@ def aggregation(argument, command_option):
                         else: # 簡易表示
                             a1 = my_score.query("playtime == @playtime").to_dict(orient = "records")[0]
                             a2 = vs_score.query("playtime == @playtime").to_dict(orient = "records")[0]
-                            msg2[vs_name][count] = textwrap.dedent(f"""
+                            tmp_msg[vs_name][count] = textwrap.dedent(f"""
                                 {"*【戦績】*" if count == 0 else ""}
                                 {playtime.replace("-", "/")} {"(2ゲスト戦)" if guest_count >= 2 else ""}
                                 \t{a1["表示名"]}： {a1["rank"]}位 {a1["rpoint"] * 100:>7} 点 ({a1["point"]:>+5.1f}pt) {a1["grandslam"]}
                                 \t{a2["表示名"]}： {a2["rank"]}位 {a2["rpoint"] * 100:>7} 点 ({a2["point"]:>+5.1f}pt) {a2["grandslam"]}
                             """).replace("-", "▲").strip() + " \n"
                         count += 1
+                        df_data = current_game if df_data.empty else pd.concat([df_data, current_game])
         else: # 対戦記録なし
-            msg2[vs_name]["info"] = "【{} vs {}】\n\t対戦相手が見つかりません。\n".format(
+            tmp_msg[vs_name]["info"] = "【{} vs {}】\n\t対戦相手が見つかりません。\n".format(
                 c.member.NameReplace(my_name, command_option, add_mark = True),
                 c.member.NameReplace(vs_name, command_option, add_mark = True),
             )
 
-    return(msg1, msg2)
+    # --- データ整列&まとめ
+    msg2 = {}
+    for m in tmp_msg.keys():
+        msg2[f"{m}_info"] = tmp_msg[m].pop("info")
+        for x in sorted(tmp_msg[m].keys()):
+            msg2[f"{m}_{x}"] = tmp_msg[m][x]
+        msg2[f"{m}_separate"] = "\n\n"
+
+    # --- ファイル出力用データ整形
+    if len(df_data) != 0:
+        df_data["プレイヤー名"] = df_data["表示名"].apply(lambda x: x.strip())
+        df_data["座席"] = df_data["seat"].apply(lambda x: ["東家", "南家", "西家", "北家"][x - 1])
+        df_data["素点"] = df_data["rpoint"] * 100
+    df_data.rename(
+        columns = {
+            "playtime": "日時",
+            "point": "獲得ポイント",
+            "rank": "順位",
+            "grandslam": "役満和了",
+        }, inplace = True)
+    df_data = df_data.filter(
+        items = ["日時", "座席", "プレイヤー名", "順位", "素点", "獲得ポイント", "役満和了"]
+    ).drop_duplicates()
+
+    namelist = list(param["competition_list"].values())
+    df_vs["対戦相手"] = df_vs["vs_表示名"].apply(lambda x: x.strip())
+    df_vs.rename(
+        columns = {
+            "results": "対戦結果", "win%": "勝率",
+            "my_point_sum": "獲得ポイント(自分)", "my_point_avg": "平均ポイント(自分)",
+            "vs_point_sum": "獲得ポイント(相手)", "vs_point_avg": "平均ポイント(相手)",
+            "my_rpoint_avg": "平均素点(自分)", "vs_rpoint_avg": "平均素点(相手)",
+            "my_rank_avg": "平均順位(自分)", "my_rank_distr": "順位分布(自分)",
+            "vs_rank_avg": "平均順位(相手)", "vs_rank_distr": "順位分布(相手)",
+        }, inplace = True)
+    df_vs2 = df_vs.query("vs_name == @namelist").filter(
+            items = ["対戦相手", "対戦結果", "勝率",
+                    "獲得ポイント(自分)", "平均ポイント(自分)",
+                    "獲得ポイント(相手)", "平均ポイント(相手)",
+                    "平均素点(自分)", "平均素点(相手)",
+                    "順位分布(自分)", "平均順位(自分)",
+                    "順位分布(相手)", "平均順位(相手)",
+                    ]
+        ).drop_duplicates()
+
+
+    return(msg1, msg2, df_data, df_vs2)
