d5f1dfe030f24415c6c515659788ca924de1815a
togakushi <togakushi>
2024-07-27 08:01:18 +0900

    matchを整理

--- lib/command/slash.py
+++ lib/command/slash.py
@@ -4,25 +4,21 @@ import lib.function as f
 from lib.function import global_value as g
 
 
-def subcommand_list(subcommand):
+class CommandCheck(str):
     """
-    サブコマンドのリストを返す
-
-    Parameters
-    ----------
-    subcommand : str
-        サブコマンド名
-
-    Returns
-    -------
-    commandlist : list
-        エイリアスを含めたサブコマンドのリスト
+    キーワードがサブコマンドかチェックする(match専用)
     """
 
-    commandlist = g.config["alias"].get(subcommand, "")
-    commandlist = [subcommand] + [x for x in commandlist.split(",") if x]
+    def __init__(self, command_name: str):
+        self.command_name = command_name
 
-    return(commandlist)
+    def __eq__(self, chk_pattern):
+        commandlist = g.config["alias"].get(chk_pattern, "")
+        commandlist = [chk_pattern] + [x for x in commandlist.split(",") if x]
+
+        if self.command_name in commandlist:
+            return(True)
+        return(False)
 
 
 @g.app.command(g.slash_command)
@@ -36,57 +32,51 @@ def slash_command(ack, body, client):
         subcom = body["text"].split()[0].lower()
         argument = body["text"].split()[1:]
 
-        match subcom:
+        match CommandCheck(subcom):
             # 成績管理系コマンド
-            case subcom if subcom in subcommand_list("results"):
-                g.logging.info(f"subcommand({subcom}): {argument}")
+            case "results":
                 c.results.slackpost.main(client, channel_id, argument)
-            case subcom if subcom in subcommand_list("graph"):
-                g.logging.info(f"subcommand({subcom}): {argument}")
+            case "graph":
                 c.graph.slackpost.main(client, channel_id, argument)
-            case subcom if subcom in subcommand_list("ranking"):
-                g.logging.info(f"subcommand({subcom}): {argument}")
+            case "ranking":
                 c.ranking.slackpost.main(client, channel_id, argument)
-            case subcom if subcom in subcommand_list("report"):
-                g.logging.info(f"subcommand({subcom}): {argument}")
+            case "report":
                 c.report.slackpost.main(client, channel_id, argument)
 
             # データベース関連コマンド
-            case subcom if subcom in subcommand_list("check"):
-                g.logging.info(f"subcommand({subcom}): {argument}")
+            case "check":
                 d.comparison.main(client, channel_id, event_ts, argument)
-            case subcom if subcom in subcommand_list("download"):
-                g.logging.info(f"subcommand({subcom}): {g.database_file}")
+            case "download":
                 f.slack_api.post_fileupload(client, channel_id, "resultdb", g.database_file)
 
             # メンバー管理系コマンド
-            case subcom if subcom in subcommand_list("member"):
+            case "member":
                 title, msg = c.member.Getmemberslist()
                 f.slack_api.post_text(client, channel_id, event_ts, title, msg)
-            case subcom if subcom in subcommand_list("add"):
+            case "add":
                 msg = c.member.MemberAppend(argument)
                 f.slack_api.post_message(client, channel_id, msg)
-            case subcom if subcom in subcommand_list("del"):
+            case "del":
                 msg = c.member.MemberRemove(argument)
                 f.slack_api.post_message(client, channel_id, msg)
 
             # チーム管理系コマンド
-            case subcom if subcom in subcommand_list("team_create"):
+            case "team_create":
                 msg = c.team.create(argument)
                 f.slack_api.post_message(client, channel_id, msg)
-            case subcom if subcom in subcommand_list("team_del"):
+            case "team_del":
                 msg = c.team.delete(argument)
                 f.slack_api.post_message(client, channel_id, msg)
-            case subcom if subcom in subcommand_list("team_add"):
+            case "team_add":
                 msg = c.team.append(argument)
                 f.slack_api.post_message(client, channel_id, msg)
-            case subcom if subcom in subcommand_list("team_remove"):
+            case "team_remove":
                 msg = c.team.remove(argument)
                 f.slack_api.post_message(client, channel_id, msg)
-            case subcom if subcom in subcommand_list("team_list"):
+            case "team_list":
                 msg = c.team.list()
                 f.slack_api.post_message(client, channel_id, msg)
-            case subcom if subcom in subcommand_list("team_clear"):
+            case "team_clear":
                 msg = c.team.clear()
                 f.slack_api.post_message(client, channel_id, msg)
 
