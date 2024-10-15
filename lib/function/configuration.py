49dafdb67496cc11633db6b4ef092c8f18b4e564
togakushi <>
2024-10-16 02:29:58 +0900

    コメント追加

--- lib/function/configuration.py
+++ lib/function/configuration.py
@@ -73,6 +73,11 @@ def setup():
     g.args = parser()
 
     # --- ログレベル追加
+    # DEBUG : 10
+    # INFO : 20
+    # WARNING : 30
+    # ERROR : 40
+    # CRITICAL : 50
     # TRACE
     logging.TRACE = 19  # type: ignore
     logging.trace = partial(logging.log, logging.TRACE)  # type: ignore
