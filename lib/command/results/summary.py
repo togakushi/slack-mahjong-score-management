259d228501c5b9e6f9de7cd46e3c82b1c3c244a5
togakushi <>
2024-10-23 01:56:40 +0900

    空白調整

--- lib/command/results/summary.py
+++ lib/command/results/summary.py
@@ -127,7 +127,7 @@ def aggregation():
             maxheadercolwidths=8,
             floatfmt=("", "+.1f", "+.1f", "", ".2f")
         )
-        msg[s_line] = "```\n" + re.sub(r" -([0-9]+)", r"▲\1", t) + "```\n"  # マイナスを記号に置換
+        msg[s_line] = "```\n" + re.sub(r" -([0-9]+)", r" ▲\1", t) + "\n```\n"  # マイナスを記号に置換
 
     # メモ追加
     if msg_memo:
