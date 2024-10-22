84263e89cb1e2829c72a9edc605c132331698eec
togakushi <>
2024-10-23 00:58:28 +0900

    集約文字数を微調整

--- lib/function/slack_api.py
+++ lib/function/slack_api.py
@@ -58,8 +58,8 @@ def post_multi_message(msg, ts=False, summarize=True):
                 key_list = list(msg.keys())
                 post_msg = msg[key_list[0]]
                 for i in key_list[1:]:
-                    # 4000文字を超える直前までまとめる
-                    if len((post_msg + msg[i])) < 4000:
+                    # 3800文字を超える直前までまとめる
+                    if len((post_msg + msg[i])) < 3800:
                         post_msg += msg[i]
                     else:
                         post_message(post_msg, ts)
