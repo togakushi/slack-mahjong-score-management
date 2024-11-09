6bfaebbc393cd00d72bf749b894e4bd1549684ee
togakushi <>
2024-11-10 01:32:08 +0900

    レーティング推移グラフ追加

---
+++ lib/command/graph/rating.py
@@ -0,0 +1,97 @@
+import logging
+import math
+import os
+
+import matplotlib.font_manager as fm
+import matplotlib.pyplot as plt
+
+import global_value as g
+from lib import command as c
+from lib import database as d
+from lib import function as f
+
+mlogger = logging.getLogger("matplotlib")
+mlogger.setLevel(logging.WARNING)
+
+
+def plot():
+    """
+    レーティング推移グラフを生成する
+
+    Returns
+    -------
+    game_count : int
+        グラフにプロットしたゲーム数
+
+    text : text
+        検索結果が0件のときのメッセージ or
+        グラフ画像保存パス
+    """
+
+    plt.close()
+    # データ収集
+    game_info = d.aggregate.game_info()
+    df_ratings = d.aggregate.calculation_rating()
+
+    if df_ratings.empty:
+        return (0, f.message.no_hits())
+
+    # 足切り
+    if g.opt.stipulated == 0:  # 規定打数が指定されない場合はレートから計算
+        g.opt.stipulated = (
+            math.ceil(game_info["game_count"] * g.opt.stipulated_rate) + 1
+        )
+    df_dropped = df_ratings.dropna(axis=1, thresh=g.opt.stipulated).ffill()
+
+    # ゲスト置換
+    for player in df_dropped.columns:
+        if player not in g.member_list:
+            if g.opt.unregistered_replace:
+                df_dropped = df_dropped.drop(columns=[player])
+            else:
+                df_dropped = df_dropped.rename(
+                    columns={
+                        player: c.member.name_replace(player, add_mark=True)
+                    }
+                )
+
+    # 並び変え
+    sorted_columns = df_dropped.iloc[-1].sort_values(ascending=False).index
+    df_sorted = df_dropped[sorted_columns]
+
+    # --- グラフ生成
+    f.common.graph_setup(plt, fm)
+
+    save_file = os.path.join(
+        g.cfg.setting.work_dir,
+        f"{g.opt.filename}.png" if g.opt.filename else "rating.png",
+    )
+
+    title_text = f"レーティング推移 ({g.prm.starttime_hm} - {g.prm.endtime_hm})"
+
+    legend_text = []
+    count = 1
+    for name, rate in df_sorted.iloc[-1].items():
+        legend_text.append(f"{count:2d}位： {name} （{rate:.1f}）")
+        count += 1
+
+    # ---
+    df_sorted.plot(
+        figsize=(21, 8)
+    )
+    plt.title(title_text, fontsize=16)
+    plt.legend(
+        legend_text,
+        bbox_to_anchor=(1, 1),
+        loc="upper left",
+        borderaxespad=0.5,
+        ncol=int(len(sorted_columns) / 25 + 1),
+    )
+    plt.xticks(
+        rotation=45,
+        ha="right",
+    )
+
+    plt.savefig(save_file, bbox_inches="tight")
+
+    return (len(df_sorted), save_file)
--- lib/command/graph/slackpost.py
+++ lib/command/graph/slackpost.py
@@ -1,6 +1,6 @@
 import global_value as g
 from lib import function as f
-from lib.command.graph import personal, summary
+from lib.command.graph import personal, rating, summary
 
 
 def main():
@@ -11,15 +11,22 @@ def main():
     g.opt.initialization("graph", g.msg.argument)
     g.prm.update(g.opt)
 
-    if len(g.prm.player_list) == 1:  # 対象がひとり → 個人成績
+    if len(g.prm.player_list) == 1:  # 対象がひとり
+        title = "個人成績"
         count, ret = personal.plot()
-    else:  # 対象が複数 → 比較
-        if g.opt.order:
-            count, ret = summary.rank_plot()
+    else:  # 対象が複数
+        if g.opt.rating:  # レーティング
+            title = "レーティング推移"
+            count, ret = rating.plot()
         else:
-            count, ret = summary.point_plot()
+            if g.opt.order:
+                title = "順位変動"
+                count, ret = summary.rank_plot()
+            else:
+                title = "ポイント推移"
+                count, ret = summary.point_plot()
 
     if count == 0:
         f.slack_api.post_message(ret)
     else:
-        f.slack_api.post_fileupload("成績グラフ", ret)
+        f.slack_api.post_fileupload(title, ret)
