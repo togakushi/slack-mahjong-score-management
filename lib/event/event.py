0f00553e21e1c3cead7b0cc60c7ebd72f0fc14de
togakushi <>
2025-01-30 23:08:16 +0900

    判定式を修正

--- lib/event/event.py
+++ lib/event/event.py
@@ -76,24 +76,25 @@ def handle_message_events(client, body):
                 detection = f.search.pattern(g.msg.text)
                 match g.msg.status:
                     case "message_append":
+                        print(f">>> {g.cfg.setting.thread_report=} {g.msg.in_thread=} {g.msg.thread_ts=}")
                         if detection:
                             if g.msg.updatable:
-                                if g.cfg.setting.thread_report == g.msg.in_thread or not g.msg.in_thread:
-                                    f.slack_api.post_message(f.message.reply(message="inside_thread", mention=True), g.msg.event_ts)
-                                else:
+                                if g.cfg.setting.thread_report == g.msg.in_thread or g.msg.thread_ts == "0":
                                     d.common.db_insert(detection, g.msg.event_ts)
+                                else:
+                                    f.slack_api.post_message(f.message.reply(message="inside_thread", mention=True), g.msg.event_ts)
                             else:
                                 f.slack_api.post_message(f.message.reply(message="restricted_channel", mention=True), g.msg.event_ts)
                     case "message_changed":
                         if detection:
                             if g.msg.updatable:
-                                if g.cfg.setting.thread_report == g.msg.in_thread or not g.msg.in_thread:
-                                    f.slack_api.post_message(f.message.reply(message="inside_thread", mention=True), g.msg.event_ts)
-                                else:
+                                if g.cfg.setting.thread_report == g.msg.in_thread or g.msg.thread_ts == "0":
                                     if d.common.exsist_record(g.msg.event_ts):
                                         d.common.db_update(detection, g.msg.event_ts)
                                     else:
                                         d.common.db_insert(detection, g.msg.event_ts)
+                                else:
+                                    f.slack_api.post_message(f.message.reply(message="inside_thread", mention=True), g.msg.event_ts)
                             else:
                                 f.slack_api.post_message(f.message.reply(message="restricted_channel", mention=True), g.msg.event_ts)
                         else:
