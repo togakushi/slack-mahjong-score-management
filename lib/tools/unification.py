82fc6130c750c096aeeb30f7cca7a1db6713391d
togakushi <>
2025-02-17 00:58:05 +0900

    禁則ルールを適応

--- lib/tools/unification.py
+++ lib/tools/unification.py
@@ -3,6 +3,7 @@ import logging
 import sqlite3
 
 import lib.global_value as g
+from lib import command as c
 from lib import database as d
 
 
@@ -12,6 +13,7 @@ def main():
 
     rename_conf = configparser.ConfigParser()
     rename_conf.read(g.args.unification, encoding="utf-8")
+    c.member.read_memberslist(False)
 
     if "rename" in rename_conf.sections():
         d.common.db_backup()
@@ -23,18 +25,29 @@ def main():
 
         for name in name_table.keys():
             count = 0
-            for alias in name_table[name]:
-                db.execute("update result set p1_name=? where p1_name=?;", (name, alias,))
-                count += db.execute("select changes();").fetchone()[0]
-                db.execute("update result set p2_name=? where p2_name=?;", (name, alias,))
-                count += db.execute("select changes();").fetchone()[0]
-                db.execute("update result set p3_name=? where p3_name=?;", (name, alias,))
-                count += db.execute("select changes();").fetchone()[0]
-                db.execute("update result set p4_name=? where p4_name=?;", (name, alias,))
-                count += db.execute("select changes();").fetchone()[0]
-                db.execute("update remarks set name=? where name=?;", (name, alias,))
-                count += db.execute("select changes();").fetchone()[0]
-            logging.notice(f"rename: {name_table[name]} -> {name} changed: {count}")
+            chk, msg = c.member.check_namepattern(name)
+            if chk:
+                for alias in name_table[name]:
+                    chk, msg = c.member.check_namepattern(alias)
+                    if chk:
+                        db.execute("update result set p1_name=? where p1_name=?;", (name, alias,))
+                        count += db.execute("select changes();").fetchone()[0]
+                        db.execute("update result set p2_name=? where p2_name=?;", (name, alias,))
+                        count += db.execute("select changes();").fetchone()[0]
+                        db.execute("update result set p3_name=? where p3_name=?;", (name, alias,))
+                        count += db.execute("select changes();").fetchone()[0]
+                        db.execute("update result set p4_name=? where p4_name=?;", (name, alias,))
+                        count += db.execute("select changes();").fetchone()[0]
+                        db.execute("update remarks set name=? where name=?;", (name, alias,))
+                        count += db.execute("select changes();").fetchone()[0]
+                    else:
+                        logging.warning(f"remove: {name} -> {alias} ({msg})")
+                        name_table[name].remove(alias)
+                        continue
+                logging.notice(f"rename: {name_table[name]} -> {name} changed: {count}")
+            else:
+                logging.warning(f"skip: {name} ({msg})")
+                continue
         db.commit()
         db.close()
     else:
