5647b01433c2dcd2f64af18da5d7ae3cd68ee80d
togakushi <>
2025-03-26 17:53:07 +0900

    fix arg-type

--- lib/tools/comparison.py
+++ lib/tools/comparison.py
@@ -21,7 +21,7 @@ def main():
             g.bot_id = g.app.client.auth_test()["user_id"]
             c.member.read_memberslist(False)
         except Exception as e:
-            sys.exit(e)
+            sys.exit("Error: %s" % e)
 
         count, _ = d.comparison.data_comparison()
         logging.notice(", ".join(f"{k}: {v}" for k, v in count.items()))  # type: ignore
