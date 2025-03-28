3d1abd46dabb0eddc294780bcf43b6cb0cdc2076
togakushi <>
2025-03-29 07:21:52 +0900

    dataclassを外出し

--- cls/config.py
+++ cls/config.py
@@ -8,18 +8,103 @@ import os
 import sys
 from dataclasses import dataclass, field
 from itertools import chain
-from typing import Any
+
+
+@dataclass
+class SettingSection:
+    """settingセクション初期値"""
+    slash_command: str = "/mahjong"
+    thread_report: bool = True
+    guest_mark: str = "※"
+    reaction_ok: str = "ok"
+    reaction_ng: str = "ng"
+    font_file: str = "ipaexg.ttf"
+    work_dir: str = "work"
+    ignore_userid: list = field(default_factory=list)
+
+
+@dataclass
+class SearchSection:
+    """searchセクション初期値"""
+    keyword: str = "終局"
+    channel: str | None = None
+    after: int = 7
+    wait: int = 180
+
+
+@dataclass
+class DatabaseSection:
+    """databaseセクション初期値"""
+    database_file: str = "mahjong.db"
+    channel_limitations: str = str()
+    backup_dir: str | None = None
+
+
+@dataclass
+class MemberSection:
+    """memberセクション初期値"""
+    registration_limit: int = 255
+    character_limit: int = 8
+    alias_limit: int = 16
+    guest_name: str = "ゲスト"
+
+
+@dataclass
+class TeamSection:
+    """teamセクション初期値"""
+    registration_limit: int = 255
+    character_limit: int = 16
+    member_limit: int = 16
+    friendly_fire: bool = True
+
+
+@dataclass
+class AliasSection:
+    """aliasセクション初期値"""
+    results: list = field(default_factory=list)
+    graph: list = field(default_factory=list)
+    ranking: list = field(default_factory=list)
+    report: list = field(default_factory=list)
+    check: list = field(default_factory=list)
+    download: list = field(default_factory=list)
+    member: list = field(default_factory=list)
+    add: list = field(default_factory=list)
+    delete: list = field(default_factory=list)
+
+
+@dataclass
+class CommandWord:
+    """チャンネル内呼び出しキーワード初期値"""
+    help: str = "ヘルプ"
+    results: str = "麻雀成績"
+    graph: str = "麻雀グラフ"
+    ranking: str = "麻雀ランキング"
+    report: str = "麻雀成績レポート"
+    member: str = "メンバー一覧"
+    team: str = "チーム一覧"
+    remarks_word: str = "麻雀成績メモ"
+    check: str = "麻雀成績チェック"
+
+
+@dataclass
+class DropItems:
+    """非表示項目リスト"""
+    results: list = field(default_factory=list)
+    ranking: list = field(default_factory=list)
+    report: list = field(default_factory=list)
 
 
 class Config():
     """コンフィグ解析クラス"""
     def __init__(self, filename: str | None = None) -> None:
-        self.setting: Any
-        self.alias: Any
-        self.db: Any
-        self.search: Any
-        self.member: Any
-        self.team: Any
+        self.setting: SettingSection
+        self.search: SearchSection
+        self.db: DatabaseSection
+        self.member: MemberSection
+        self.team: TeamSection
+        self.alias: AliasSection
+        self.dropitems: DropItems
+        self.cw: CommandWord
 
         self.config = configparser.ConfigParser()
         if filename is not None:
@@ -51,26 +136,66 @@ class Config():
                 self.config.add_section(x)
 
         # セクション読み込み
-        self.setting_section()
-        self.search_section()
-        self.database_section()
-        self.member_section()
-        self.team_section()
-        self.alias_section()
-
-        @dataclass
-        class CommandWord:
-            """チャンネル内呼び出しキーワード"""
-            help: str = self.config["help"].get("commandword", "ヘルプ")
-            results: str = self.config["results"].get("commandword", "麻雀成績")
-            graph: str = self.config["graph"].get("commandword", "麻雀グラフ")
-            ranking: str = self.config["ranking"].get("commandword", "麻雀ランキング")
-            report: str = self.config["report"].get("commandword", "麻雀成績レポート")
-            member: str = self.config["member"].get("commandword", "メンバー一覧")
-            team: str = self.config["team"].get("commandword", "チーム一覧")
-            remarks_word: str = self.config["setting"].get("remarks_word", "麻雀成績メモ")
-            check: str = self.config["database"].get("commandword", "麻雀成績チェック")
-        self.cw = CommandWord()
+        self.setting = SettingSection(
+            slash_command=self.config["setting"].get("slash_commandname", SettingSection.slash_command),
+            thread_report=self.config["setting"].getboolean("thread_report", SettingSection.thread_report),
+            guest_mark=self.config["setting"].get("guest_mark", SettingSection.guest_mark),
+            reaction_ok=self.config["setting"].get("reaction_ok", SettingSection.reaction_ok),
+            reaction_ng=self.config["setting"].get("reaction_ng", SettingSection.reaction_ng),
+            font_file=self.config["setting"].get("font_file", SettingSection.font_file),
+            work_dir=self.config["setting"].get("work_dir", SettingSection.work_dir),
+            ignore_userid=[x.strip() for x in self.config["setting"].get("ignore_userid", "").split(",")],
+        )
+        self.search = SearchSection(
+            keyword=self.config["search"].get("keyword", SearchSection.keyword),
+            channel=self.config["search"].get("channel", SearchSection.channel),
+            after=self.config["search"].getint("after", SearchSection.after),
+            wait=self.config["search"].getint("wait", SearchSection.wait),
+        )
+        self.db = DatabaseSection(
+            database_file=self.config["database"].get("database_file", DatabaseSection.database_file),
+            channel_limitations=self.config["database"].get("channel_limitations", DatabaseSection.channel_limitations),
+            backup_dir=self.config["database"].get("backup_dir", DatabaseSection.backup_dir),
+        )
+        self.member = MemberSection(
+            registration_limit=self.config["member"].getint("registration_limit", MemberSection.registration_limit),
+            character_limit=self.config["member"].getint("character_limit", MemberSection.character_limit),
+            alias_limit=self.config["member"].getint("alias_limit", MemberSection.alias_limit),
+            guest_name=self.config["member"].get("guest_name", MemberSection.guest_name),
+        )
+        self.team = TeamSection(
+            registration_limit=self.config["team"].getint("registration_limit", TeamSection.registration_limit),
+            character_limit=self.config["team"].getint("character_limit", TeamSection.character_limit),
+            member_limit=self.config["team"].getint("member_limit", TeamSection.member_limit),
+            friendly_fire=self.config["team"].getboolean("friendly_fire", TeamSection.friendly_fire),
+        )
+        self.alias = AliasSection(
+            results=[x.strip() for x in self.config["alias"].get("results", "").split(",")],
+            graph=[x.strip() for x in self.config["alias"].get("graph", "").split(",")],
+            ranking=[x.strip() for x in self.config["alias"].get("ranking", "").split(",")],
+            report=[x.strip() for x in self.config["alias"].get("report", "").split(",")],
+            check=[x.strip() for x in self.config["alias"].get("check", "").split(",")],
+            download=[x.strip() for x in self.config["alias"].get("download", "").split(",")],
+            member=[x.strip() for x in self.config["alias"].get("member", "").split(",")],
+            add=[x.strip() for x in self.config["alias"].get("add", "").split(",")],
+            delete=[x.strip() for x in self.config["alias"].get("del", "").split(",")],
+        )
+        self.cw = CommandWord(  # チャンネル内呼び出しキーワード
+            help=self.config["help"].get("commandword", CommandWord.help),
+            results=self.config["results"].get("commandword", CommandWord.results),
+            graph=self.config["graph"].get("commandword", CommandWord.graph),
+            ranking=self.config["ranking"].get("commandword", CommandWord.ranking),
+            report=self.config["report"].get("commandword", CommandWord.report),
+            member=self.config["member"].get("commandword", CommandWord.member),
+            team=self.config["team"].get("commandword", CommandWord.team),
+            remarks_word=self.config["setting"].get("remarks_word", CommandWord.remarks_word),
+            check=self.config["database"].get("commandword", CommandWord.check),
+        )
+        self.dropitems = DropItems(  # 非表示項目リスト
+            results=[x.strip() for x in self.config["results"].get("dropitems", "").split(",")],
+            ranking=[x.strip() for x in self.config["ranking"].get("dropitems", "").split(",")],
+            report=[x.strip() for x in self.config["report"].get("dropitems", "").split(",")],
+        )
 
         # サブコマンドデフォルト
         self.results = self.command_default("results")
@@ -78,18 +203,9 @@ class Config():
         self.ranking = self.command_default("ranking")
         self.report = self.command_default("report")
 
-        @dataclass
-        class DropItems:
-            """非表示項目リスト"""
-            results: list = field(default_factory=list)
-            ranking: list = field(default_factory=list)
-            report: list = field(default_factory=list)
-        self.dropitems = DropItems()
-        self.dropitems.results = [x.strip() for x in self.config["results"].get("dropitems", "").split(",")]
-        self.dropitems.ranking = [x.strip() for x in self.config["ranking"].get("dropitems", "").split(",")]
-        self.dropitems.report = [x.strip() for x in self.config["report"].get("dropitems", "").split(",")]
-
+        # その他/更新
         self.undefined_word: int = self.config["regulations"].getint("undefined", 2)
+        self.setting.work_dir = os.path.join(os.path.realpath(os.path.curdir), self.setting.work_dir)
 
         logging.info("setting=%s", vars(self.setting))
         logging.info("search=%s", vars(self.search))
@@ -97,91 +213,6 @@ class Config():
         logging.info("alias=%s", vars(self.alias))
         logging.info("commandword=%s", vars(self.cw))
         logging.info("dropitems=%s", vars(self.dropitems))
-
-    def setting_section(self):
-        """settingセクション読み込み"""
-        @dataclass
-        class Setting:
-            """初期化"""
-            slash_command: str = self.config["setting"].get("slash_commandname", "/mahjong")
-            thread_report: bool = self.config["setting"].getboolean("thread_report", True)
-            guest_mark: str = self.config["setting"].get("guest_mark", "※")
-            reaction_ok: str = self.config["setting"].get("reaction_ok", "ok")
-            reaction_ng: str = self.config["setting"].get("reaction_ng", "ng")
-            font_file: str = self.config["setting"].get("font_file", "ipaexg.ttf")
-            work_dir: str = self.config["setting"].get("work_dir", "work")
-            ignore_userid: list = field(default_factory=list)
-        self.setting = Setting()
-        self.setting.ignore_userid = [x.strip() for x in self.config["setting"].get("ignore_userid", "").split(",")]
-        self.setting.work_dir = os.path.join(os.path.realpath(os.path.curdir), self.setting.work_dir)
-
-    def search_section(self):
-        """searchセクション読み込み"""
-        @dataclass
-        class Search:
-            """初期化"""
-            keyword: str = self.config["search"].get("keyword", "終局")
-            channel: str | None = self.config["search"].get("channel", None)
-            after: int = self.config["search"].getint("after", 7)
-            wait: int = self.config["search"].getint("wait", 180)
-        self.search = Search()
-
-    def database_section(self):
-        """databaseセクション読み込み"""
-        @dataclass
-        class Database:
-            """初期化"""
-            database_file: str = self.config["database"].get("database_file", "mahjong.db")
-            channel_limitations: str = self.config["database"].get("channel_limitations", "")
-            backup_dir: str | None = self.config["database"].get("backup_dir", None)
-        self.db = Database()
-
-    def member_section(self):
-        """memberセクション読み込み"""
-        @dataclass
-        class Member:
-            """初期化"""
-            registration_limit: int = self.config["member"].getint("registration_limit", 255)
-            character_limit: int = self.config["member"].getint("character_limit", 8)
-            alias_limit: int = self.config["member"].getint("alias_limit", 16)
-            guest_name: str = self.config["member"].get("guest_name", "ゲスト")
-        self.member = Member()
-
-    def team_section(self):
-        """teamセクション読み込み"""
-        @dataclass
-        class Team:
-            """初期化"""
-            registration_limit: int = self.config["team"].getint("registration_limit", 255)
-            character_limit: int = self.config["team"].getint("character_limit", 16)
-            member_limit: int = self.config["team"].getint("member_limit", 16)
-            friendly_fire: bool = self.config["team"].getboolean("friendly_fire", True)
-        self.team = Team()
-
-    def alias_section(self):
-        """aliasセクション読み込み"""
-        @dataclass
-        class Alias:
-            """初期化"""
-            results: list = field(default_factory=list)
-            graph: list = field(default_factory=list)
-            ranking: list = field(default_factory=list)
-            report: list = field(default_factory=list)
-            check: list = field(default_factory=list)
-            download: list = field(default_factory=list)
-            member: list = field(default_factory=list)
-            add: list = field(default_factory=list)
-            delete: list = field(default_factory=list)
-        self.alias = Alias()
-        self.alias.results = [x.strip() for x in self.config["alias"].get("results", "").split(",")]
-        self.alias.graph = [x.strip() for x in self.config["alias"].get("graph", "").split(",")]
-        self.alias.ranking = [x.strip() for x in self.config["alias"].get("ranking", "").split(",")]
-        self.alias.report = [x.strip() for x in self.config["alias"].get("report", "").split(",")]
-        self.alias.check = [x.strip() for x in self.config["alias"].get("check", "").split(",")]
-        self.alias.download = [x.strip() for x in self.config["alias"].get("download", "").split(",")]
-        self.alias.member = [x.strip() for x in self.config["alias"].get("member", "").split(",")]
-        self.alias.add = [x.strip() for x in self.config["alias"].get("add", "").split(",")]
-        self.alias.delete = [x.strip() for x in self.config["alias"].get("del", "").split(",")]
 
     def command_default(self, section):
         """設定ファイルのセクションを読み込みインスタンス化して返す
