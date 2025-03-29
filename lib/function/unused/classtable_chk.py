54807e5e8687061c5a95bbf2d5744056d56093cd
togakushi <>
2025-03-29 20:31:18 +0900

    fix pylint

--- lib/function/unused/classtable_chk.py
+++ lib/function/unused/classtable_chk.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+"""
+unused/classtable_chk.py
+"""
 
 import json
 import random
 
-tablefile = "classtable_mahjongsoul.json"
-tablefile = "classtable_tenho.json"
+TABLE_FILE = "classtable_mahjongsoul.json"
+TABLE_FILE = "classtable_tenho.json"
 
-with open(tablefile, encoding="utf-8") as f:
+with open(TABLE_FILE, encoding="utf-8") as f:
     class_data = json.load(f)
 
 for i in range(len(class_data["table"])):
@@ -14,6 +17,7 @@ for i in range(len(class_data["table"])):
 
 
 def d(dani, point, rank):
+    """段位算出"""
     get_point = class_data["table"][dani]["acquisition"][rank - 1]
     new_point = point + get_point
 
@@ -43,10 +47,10 @@ def d(dani, point, rank):
     return (new_point, dani)
 
 
-p = 0
-dani = 0
-r = None
-rank_list = [
+P = 0
+DANI = 0
+R = None
+RANK_LIST = [
     4, 3, 4, 4, 1, 1, 4, 4, 1, 1, 2, 1, 1, 2, 2, 2, 1, 2, 4, 2, 3, 3, 1, 1, 3, 3, 3, 3, 4, 1, 4, 3, 3, 2, 1, 3, 2, 1, 2, 2,  # 40
     1, 2, 2, 2, 1, 2, 3, 1, 4, 2, 2, 2, 2, 4, 2, 2, 1, 1, 2, 1, 3, 2, 2, 3, 4, 3, 1, 4, 1, 2, 3, 2, 1, 3, 3, 4, 2, 1, 3, 1,  # 80
     3, 4, 2, 3, 4, 2, 3, 3, 2, 1, 3, 3, 2, 1, 1, 3, 2, 1, 1, 2, 4, 1, 1, 1, 1, 2, 4, 2, 3, 3, 2, 4, 1, 4, 1, 4, 2, 2, 2, 2,  # 120
@@ -56,5 +60,5 @@ rank_list = [
 ]
 # for r in rank_list:
 for i in range(1000):
-    r = random.choice([1, 3, 2, 4, 1, 4])
-    p, dani = d(dani, p, r)
+    R = random.choice([1, 3, 2, 4, 1, 4])
+    P, DANI = d(DANI, P, R)
