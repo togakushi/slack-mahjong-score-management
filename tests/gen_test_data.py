0359c380282ef8b9b9ac2f17c223afaeafea333e
togakushi <>
2025-04-27 08:10:43 +0900

    テストデータ生成ツール

---
+++ tests/gen_test_data.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import itertools
+import random
+import sqlite3
+from contextlib import closing
+from datetime import datetime
+
+import libs.global_value as g
+from libs.functions.tools import score_simulator
+from libs.functions import configuration, score
+
+configuration.setup()
+configuration.read_memberslist(log=False)
+
+# 対戦組み合わせ作成
+teams = [x["team"] for x in g.team_list]
+position = ["先鋒", "次鋒", "中堅", "副将", "大将"]
+teams_data: dict = {x["team"]: x["member"].split(",") for x in g.team_list}
+matchup = list(itertools.combinations(teams, 4))
+teams_count = {x: 0 for x in teams}
+total_count = 0
+season_times = 1
+
+print(f"{g.team_list=}")
+print(f"{teams_data=}")
+
+now = datetime.now().timestamp() - ((len(matchup) + 7) * 86400 * season_times)
+dt = now
+with closing(sqlite3.connect(g.cfg.db.database_file)) as cur:
+    cur.execute("delete from result;")
+    for season in range(1, season_times + 1):
+        random.shuffle(matchup)
+        for count, game in enumerate(matchup):
+            print(f">>> 第{season:02d}期 {count + 1:04d}試合 /", datetime.fromtimestamp(dt).strftime("%Y-%m-%d"))
+            # 対戦メンバーの決定
+            vs_member = {}
+            for team_name in game:
+                teams_count[team_name] += 1
+                team_member = teams_data[team_name]
+                random.shuffle(team_member)
+                vs_member[team_name] = team_member
+            # print(game, vs_member)
+
+            # 試合結果
+            total_count += 1
+            team_name = list(game)
+            random.shuffle(team_name)
+            for idx in range(5):
+                member = [
+                    vs_member[team_name[0]][idx],
+                    vs_member[team_name[1]][idx],
+                    vs_member[team_name[2]][idx],
+                    vs_member[team_name[3]][idx],
+                ]
+                random.shuffle(member)
+                param = {}
+                vs_score = score_simulator.simulate_game()
+                result = score.get_score([
+                    member[0], str(vs_score[0] / 100),
+                    member[1], str(vs_score[1] / 100),
+                    member[2], str(vs_score[2] / 100),
+                    member[3], str(vs_score[3] / 100),
+                    f"第{season:02d}期{count + 1:04d}試合_{position[idx]}戦",
+                ])
+
+                # データ投入
+                dt = now + total_count * 86400 + idx * 3600 + random.random()
+                param = {
+                    "ts": str(dt),
+                    "playtime": datetime.fromtimestamp(dt).strftime("%Y-%m-%d %H:%M:%S.%f"),
+                    "rule_version": g.cfg.mahjong.rule_version,
+                }
+                param.update(result)
+                cur.execute(g.sql["RESULT_INSERT"], param)
+
+                output = "{}: [{}位 {} / {} ({}pt)] [{}位 {} / {} ({}pt)] [{}位 {} / {} ({}pt)] [{}位 {} / {} ({}pt)]".format(
+                    position[idx],
+                    result["p1_rank"], result["p1_name"], result["p1_rpoint"] * 100, result["p1_point"],
+                    result["p2_rank"], result["p2_name"], result["p2_rpoint"] * 100, result["p2_point"],
+                    result["p3_rank"], result["p3_name"], result["p3_rpoint"] * 100, result["p3_point"],
+                    result["p4_rank"], result["p4_name"], result["p4_rpoint"] * 100, result["p4_point"],
+                )
+                # print(output)
+
+    cur.commit()
+
+print(teams_count)
