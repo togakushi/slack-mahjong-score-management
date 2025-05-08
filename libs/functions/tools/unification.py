c967aed938f9e0d9a95bed8c4698a4056f1237ed
togakushi <>
2025-05-08 11:14:51 +0900

    DB修正処理追加

--- libs/functions/tools/unification.py
+++ libs/functions/tools/unification.py
@@ -18,8 +18,8 @@ def main():
     rename_conf.read(g.args.unification, encoding="utf-8")
     configuration.read_memberslist(False)
 
+    modify.db_backup()
     if "rename" in rename_conf.sections():
-        modify.db_backup()
         name_table: dict = {}
         for name, alias in rename_conf["rename"].items():
             name_table.setdefault(name, [x.strip() for x in alias.split(",")])
@@ -54,4 +54,21 @@ def main():
         db.commit()
         db.close()
     else:
-        logging.error("section not found.")
+        db = sqlite3.connect(g.cfg.db.database_file)
+        db.row_factory = sqlite3.Row
+        for alias, name in g.member_list.items():
+            if alias == name:
+                continue
+            rows = db.execute("select ts, p1_name, p2_name, p3_name, p4_name from result where ? in (p1_name, p2_name, p3_name, p4_name);", (alias,))
+            for row in rows:
+                match alias:
+                    case x if x == row["p1_name"]:
+                        db.execute("update result set p1_name=? where p1_name=? and ts=?;", (name, alias, row["ts"]))
+                    case x if x == row["p2_name"]:
+                        db.execute("update result set p2_name=? where p2_name=? and ts=?;", (name, alias, row["ts"]))
+                    case x if x == row["p3_name"]:
+                        db.execute("update result set p3_name=? where p3_name=? and ts=?;", (name, alias, row["ts"]))
+                    case x if x == row["p4_name"]:
+                        db.execute("update result set p4_name=? where p4_name=? and ts=?;", (name, alias, row["ts"]))
+        db.commit()
+        db.close()
