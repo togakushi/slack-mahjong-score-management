73beb71bd8397266d1efc3a5d080d9e4d091f9e3
togakushi <>
2025-05-29 00:56:35 +0900

    テストケース追加

---
+++ tests/name_check/param_data.py
@@ -0,0 +1,27 @@
+"""
+テスト用パラメータ
+"""
+
+from typing import Any, Tuple
+
+
+# 名前チェック
+flag_name_pattern_01: dict[str, Tuple[Any, ...]] = {
+    # input_args, expected_flags
+    "kana": ("カタカナ", True),
+    "hira": ("ひらがな", True),
+    "alphabet": ("ｘｙｚ", True),
+}
+
+flag_name_pattern_02: dict[str, Tuple[Any, ...]] = {
+    # input_args, expected_flags
+    "guest kana": ("ゲスト", False),
+    "guest hira": ("げすと", False),
+    "guest mix": ("げスと", False),
+    "prohibited character 01": ("<名前>", False),
+    "prohibited character 02": ("****", False),
+    "over number": ("0123456789", False),
+    "registered name 01": ("宮永咲", False),
+    "registered name 02": ("宮永咲ちゃん", False),
+    "registered team": ("清澄高校", False),
+}
---
+++ tests/name_check/test_name.py
@@ -0,0 +1,43 @@
+"""
+tests/test_parser.py
+"""
+
+import sys
+
+import pytest
+
+from libs.functions import configuration
+from libs.utils import validator
+from tests.name_check import param_data
+
+TEST_ARGS = ["progname", "--config=tests/test_data/saki.ini"]
+
+
+@pytest.mark.parametrize(
+    "input_args, expected_flags",
+    list(param_data.flag_name_pattern_01.values()),
+    ids=list(param_data.flag_name_pattern_01.keys()),
+)
+def test_name_permit(input_args, expected_flags, monkeypatch):
+    """メンバー登録テスト(OK)"""
+    monkeypatch.setattr(sys, "argv", TEST_ARGS)
+    configuration.setup()
+    configuration.read_memberslist()
+
+    flg, _ = validator.check_namepattern(input_args)
+    assert flg == expected_flags
+
+
+@pytest.mark.parametrize(
+    "input_args, expected_flags",
+    list(param_data.flag_name_pattern_02.values()),
+    ids=list(param_data.flag_name_pattern_02.keys()),
+)
+def test_name_refusal(input_args, expected_flags, monkeypatch):
+    """メンバー登録テスト(NG)"""
+    monkeypatch.setattr(sys, "argv", TEST_ARGS)
+    configuration.setup()
+    configuration.read_memberslist()
+
+    flg, _ = validator.check_namepattern(input_args)
+    assert flg == expected_flags
