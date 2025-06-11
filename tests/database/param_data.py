a711019a641f144fe61521d4b2529976f0ec435e
togakushi <>
2025-06-12 00:36:26 +0900

    テストパターン追加

--- tests/database/param_data.py
+++ tests/database/param_data.py
@@ -6,7 +6,7 @@ from typing import Any, Tuple
 
 # ユーザ追加
 user_add_case_01: dict[str, Tuple[Any, ...]] = {
-    # username, ret_meg, registered
+    # user_name, ret_meg, registered
     "append 01": ("めんばー１", "登録しました", True),
     "append 02": ("めんばー２", "登録しました", True),
     "unregistered member alias": ("めんばー３ 三人目", "登録されていません", False),
@@ -17,6 +17,18 @@ user_add_case_01: dict[str, Tuple[Any, ...]] = {
     "guest(kana)": ("ゲスト", "使用できない名前", True),
     "guest(hira)": ("げすと", "使用できない名前", False),
     "guest(mix)": ("げスと", "使用できない名前", False),
+    "defined range word": ("今月", "検索範囲指定に使用される単語", False),
+    "defined option word": ("ゲスト無効", "オプションに使用される単語", False),
+}
+
+# チーム追加
+team_add_case_01: dict[str, Tuple[Any, ...]] = {
+    # team_name, ret_meg, registered
+    "append 01": ("ちーむ１", "登録しました", True),
+    "duplication": ("清澄高校", "存在するチーム", True),
+    "same member": ("宮永咲", "存在するメンバー", False),
+    "invalid char": ("***", "使用できない記号", False),
+    "long name": ("a" * 100, "登録可能文字数", False),
     "defined range word": ("今月", "検索範囲指定に使用される単語", False),
     "defined option word": ("ゲスト無効", "オプションに使用される単語", False),
 }
--- tests/database/test_registration.py
+++ tests/database/test_registration.py
@@ -11,7 +11,7 @@ import libs.global_value as g
 from cls.config import Config
 from libs.data import initialization
 from libs.functions import configuration
-from libs.registry import member
+from libs.registry import member, team
 from libs.utils import dbutil
 from tests.database import param_data
 
@@ -64,13 +64,13 @@ def test_guest_name():
 
 
 @pytest.mark.parametrize(
-    "username, ret_meg, registered",
+    "user_name, ret_meg, registered",
     list(param_data.user_add_case_01.values()),
     ids=list(param_data.user_add_case_01.keys())
 )
-def test_member_add(username, ret_meg, registered):
+def test_member_add(user_name, ret_meg, registered):
     """ユーザ登録テスト"""
-    ret = member.append(str(username).split())
+    ret = member.append(str(user_name).split())
     assert ret_meg in ret
 
     with closing(dbutil.get_connection()) as conn:
@@ -78,7 +78,28 @@ def test_member_add(username, ret_meg, registered):
         rows = cur.fetchall()
         assert rows is not None
 
-    check_name = str(username).split()[0]
+    check_name = str(user_name).split()[0]
+    name_list = [dict(row).get("name") for row in rows]
+    print(f"in: {check_name} result: {ret}")
+    assert (check_name in name_list) == registered
+
+
+@pytest.mark.parametrize(
+    "team_name, ret_meg, registered",
+    list(param_data.team_add_case_01.values()),
+    ids=list(param_data.team_add_case_01.keys())
+)
+def test_team_create(team_name, ret_meg, registered):
+    """チーム作成テスト"""
+    ret = team.create(str(team_name).split())
+    assert ret_meg in ret
+
+    with closing(dbutil.get_connection()) as conn:
+        cur = conn.execute("select name from team;")
+        rows = cur.fetchall()
+        assert rows is not None
+
+    check_name = str(team_name).split()[0]
     name_list = [dict(row).get("name") for row in rows]
     print(f"in: {check_name} result: {ret}")
     assert (check_name in name_list) == registered
