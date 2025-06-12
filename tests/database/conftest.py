1e6e34736ed575229206b12f165d60be958202a4
togakushi <>
2025-06-12 15:55:32 +0900

    共通処理分離

---
+++ tests/database/conftest.py
@@ -0,0 +1,51 @@
+"""
+tests/database/conftest.py
+"""
+
+from contextlib import closing
+
+import pandas as pd
+import pytest
+
+import libs.global_value as g
+from cls.config import Config
+from libs.data import initialization
+from libs.functions import configuration
+from libs.utils import dbutil
+
+
+@pytest.fixture(scope="package")
+def shared_connection():
+    """共有インメモリDBと接続"""
+    configuration.set_loglevel()
+    g.cfg = Config("tests/testdata/minimal.ini")
+    g.cfg.db.database_file = "memdb1?mode=memory&cache=shared"
+    conn = dbutil.get_connection()
+    yield conn
+    conn.close()
+
+
+@pytest.fixture(scope="package", autouse=True)
+def initialize_database(shared_connection):
+    """DB初期化"""
+    initialization.initialization_resultdb()
+    with closing(dbutil.get_connection()) as conn:
+        pd.read_csv("tests/test_data/saki_member.csv").to_sql(
+            name="member",
+            con=conn,
+            if_exists="append",
+            index=False,
+        )
+        pd.read_csv("tests/test_data/saki_team.csv").to_sql(
+            name="team",
+            con=conn,
+            if_exists="append",
+            index=False,
+        )
+        cur = conn.execute("select name from member where id != 0;")
+        rows = cur.fetchall()
+        for name in [dict(row).get("name") for row in rows]:
+            conn.execute("insert into alias(name, member) values (?, ?);", (name, name,))
+        conn.commit()
+
+    configuration.read_memberslist()
--- tests/database/test_registration.py
+++ tests/database/test_registration.py
@@ -4,53 +4,12 @@ tests/database/test_registration.py
 
 from contextlib import closing
 
-import pandas as pd
 import pytest
 
 import libs.global_value as g
-from cls.config import Config
-from libs.data import initialization
-from libs.functions import configuration
 from libs.registry import member, team
 from libs.utils import dbutil
 from tests.database import param_data
-
-
-@pytest.fixture(scope="module")
-def shared_connection():
-    """共有インメモリDBと接続"""
-    configuration.set_loglevel()
-    g.cfg = Config("tests/testdata/minimal.ini")
-    g.cfg.db.database_file = "memdb1?mode=memory&cache=shared"
-    conn = dbutil.get_connection()
-    yield conn
-    conn.close()
-
-
-@pytest.fixture(scope="module", autouse=True)
-def initialize_database(shared_connection):
-    """DB初期化"""
-    initialization.initialization_resultdb()
-    with closing(dbutil.get_connection()) as conn:
-        pd.read_csv("tests/test_data/saki_member.csv").to_sql(
-            name="member",
-            con=conn,
-            if_exists="append",
-            index=False,
-        )
-        pd.read_csv("tests/test_data/saki_team.csv").to_sql(
-            name="team",
-            con=conn,
-            if_exists="append",
-            index=False,
-        )
-        cur = conn.execute("select name from member where id != 0;")
-        rows = cur.fetchall()
-        for name in [dict(row).get("name") for row in rows]:
-            conn.execute("insert into alias(name, member) values (?, ?);", (name, name,))
-        conn.commit()
-
-    configuration.read_memberslist()
 
 
 def test_guest_name():
