761b7141fe2d38fcbd2edadab38d0621ae036576
togakushi <>
2025-06-30 07:56:03 +0900

    コメント修正

--- libs/functions/compose/__init__.py
+++ libs/functions/compose/__init__.py
@@ -2,6 +2,8 @@
 メッセージ表示用関数
 
 - `libs.functions.compose.help.py`: ヘルプメッセージ
+- `libs.functions.compose.badge.py`: バッジ表示
+- `libs.functions.compose.text_item.py`: テキストアイテム表示
 """
 
 from libs.functions.compose import badge, msg_help, text_item
