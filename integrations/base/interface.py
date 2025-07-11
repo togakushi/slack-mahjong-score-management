642c3d644cdbe26c3b55bc43be75d470ebe23a9c
togakushi <>
2025-07-12 08:52:04 +0900

    add

--- integrations/base/interface.py
+++ integrations/base/interface.py
@@ -2,17 +2,18 @@
 
 from abc import ABC, abstractmethod
 from typing import Any
+from cls.types import MsgDict
 
 
 class ReactionsInterface(ABC):
     """リアクション操作抽象インターフェース"""
     @abstractmethod
-    def status(self, ch=None, ts=None) -> list:
+    def status(self, ch=str, ts=str) -> list:
         """botが付けたリアクションの種類を返す
 
         Args:
-            ch (str, optional): チャンネルID. Defaults to None.
-            ts (str, optional): メッセージのタイムスタンプ. Defaults to None.
+            ch (str): チャンネルID
+            ts (str): メッセージのタイムスタンプ
 
         Returns:
             list: リアクション
@@ -20,11 +21,36 @@ class ReactionsInterface(ABC):
         return []
 
     @abstractmethod
-    def all_remove(self, delete_list: list):
+    def all_remove(self, delete_list: list, ch: str):
         """すべてのリアクションを削除する
 
         Args:
             delete_list (list): 削除対象のタイムスタンプ
+            ch (str): 対象チャンネルID
+        """
+
+    @abstractmethod
+    def ok(self, ok_icon: str, ng_icon: str, ch: str, ts: str, reactions_list: list) -> None:
+        """OKリアクションを付ける
+
+        Args:
+            ok_icon (str): OKリアクション
+            ng_icon (str): NGリアクション
+            ch (str): チャンネルID
+            ts (str): タイムスタンプ
+            reactions_list (list): 付与済みリアクションリスト
+        """
+
+    @abstractmethod
+    def ng(self, ok_icon: str, ng_icon: str, ch: str, ts: str, reactions_list: list) -> None:
+        """NGリアクションを付ける
+
+        Args:
+            ok_icon (str): OKリアクション
+            ng_icon (str): NGリアクション
+            ch (str): チャンネルID
+            ts (str): タイムスタンプ
+            reactions_list (list): 付与済みリアクションリスト
         """
 
 
@@ -115,7 +141,7 @@ class APIInterface(ABC):
         """
 
     @abstractmethod
-    def get_conversations(self, ch=None, ts=None) -> dict:
+    def get_conversations(self, ch=str, ts=str) -> dict:
         """スレッド情報の取得
 
         Args:
@@ -151,3 +177,71 @@ class MessageParserInterface(ABC):
     @abstractmethod
     def check_updatable(self):
         pass
+
+
+class NewMsgParserInterface(ABC):
+    data: MsgDict
+
+    def __init__(self):
+        self.data: MsgDict = MsgDict()
+
+    @abstractmethod
+    def parser(self, body: Any):
+        pass
+
+    @property
+    def in_thread(self) -> bool:
+        if self.data["thread_ts"] == "0":
+            return False
+        elif self.data["event_ts"] == self.data["thread_ts"]:
+            return False
+        return True
+
+    @property
+    def text(self) -> str:
+        return self.data.get("text", "")
+
+    @property
+    def keyword(self) -> str:
+        if (ret := self.data["text"].split()):
+            return ret[0]
+        return ret
+
+    @property
+    def argument(self) -> list:
+        if (ret := self.data["text"].split()):
+            return ret[1:]
+        return ret
+
+    @argument.setter
+    def argument(self, value: list) -> None:
+        if isinstance(value, license):
+            self.argument = value
+
+    @property
+    def event_ts(self) -> str:
+        return self.data.get("event_ts", "0")
+
+    @event_ts.setter
+    def event_ts(self, value: str) -> None:
+        self.data["event_ts"] = value
+
+    @property
+    def thread_ts(self) -> str:
+        return self.data.get("thread_ts", "0")
+
+    @property
+    def channel_id(self) -> str:
+        return self.data.get("channel_id", "")
+
+    @property
+    def channel_type(self) -> str:
+        return self.data.get("channel_type", "dummy")
+
+    @property
+    def user_id(self) -> str:
+        return self.data.get("user_id", "")
+
+    @property
+    def status(self) -> str:
+        return self.data.get("status", "dummy")
