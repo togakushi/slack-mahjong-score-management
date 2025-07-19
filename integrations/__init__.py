7ebab71bfd29a8c766052cbe25b7d4f19d0d7a21
togakushi <>
2025-07-19 15:45:14 +0900

    ドキュメント更新

--- integrations/__init__.py
+++ integrations/__init__.py
@@ -1,5 +1,9 @@
 """統合インターフェース定義
 
+- `integrations.base`: 基底クラス
+- `integrations.standard_io`: 標準入出力インターフェース
+- `integrations.slack`: slack入出力インターフェース
+
 - `integrations.factory`: インターフェースセレクター
 - `integrations.protocols`: プロトコル定義
 """
