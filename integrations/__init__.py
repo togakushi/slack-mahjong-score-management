6cfc6b37974e6a6e29af322978baac3fc64d3754
togakushi <>
2025-09-16 23:55:27 +0900

    コメント修正

--- integrations/__init__.py
+++ integrations/__init__.py
@@ -1,8 +1,9 @@
 """統合インターフェース定義
 
 - `integrations.base`: 基底クラス
+- `integrations.slack`: slackインターフェース
+- `integrations.web`: webインターフェース
 - `integrations.standard_io`: 標準入出力インターフェース
-- `integrations.slack`: slack入出力インターフェース
 - `integrations.factory`: インターフェースセレクター
 - `integrations.protocols`: プロトコル定義
 """
