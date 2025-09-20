4441afa7e2aae91c649b992ceb32c68eaae98f46
togakushi <>
2025-09-20 23:01:05 +0900

    TypeAliasに変更

--- libs/global_value.py
+++ libs/global_value.py
@@ -1,28 +1,18 @@
 """モジュール間データ共有用"""
 
-from typing import TYPE_CHECKING, Any, Callable, Literal, Union
+from typing import TYPE_CHECKING, Literal
 
 if TYPE_CHECKING:
     from argparse import Namespace
 
     from cls.config import AppConfig
-    from cls.types import TeamDataDict
-    from integrations.slack.config import AppConfig as slack_conf
-    from integrations.standard_io.config import AppConfig as stdio_conf
-    from integrations.web.config import AppConfig as web_conf
+    from cls.types import AppConfigType, TeamDataDict
 
 selected_service: Literal["slack", "web", "standard_io"] = "slack"
-app_config: Union["slack_conf", "web_conf", "stdio_conf"]
+app_config: "AppConfigType"
 
 args: "Namespace"
 """コマンドライン引数"""
-
-slash_command_name: str
-"""スラッシュコマンド名"""
-slash_commands: dict[str, Callable[..., Any]] = {}
-"""スラッシュコマンド用ディスパッチテーブル"""
-special_commands: dict[str, Callable[..., Any]] = {}
-"""個別コマンド用ディスパッチテーブル"""
 
 # モジュール共通インスタンス
 cfg: "AppConfig"
