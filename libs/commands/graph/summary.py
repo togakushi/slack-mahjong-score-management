9470147160570c0474c3e3ee09dd8270d26bb6b5
togakushi <>
2025-10-09 09:27:12 +0900

    return忘れ

--- libs/commands/graph/summary.py
+++ libs/commands/graph/summary.py
@@ -104,6 +104,7 @@ def rank_plot(m: "MessageParserProtocol"):
     if target_data.empty:  # 描写対象が0人の場合は終了
         m.post.headline = {"0": message.random_reply(m, "no_hits", False)}
         m.status.result = False
+        return
 
     if g.params.get("search_word"):
         pivot_index = "comment"
