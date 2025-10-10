6d8d4e2aade068a5a5c7bfdd858855e6124cecf5
togakushi <>
2025-10-11 07:01:36 +0900

    チェック強化

--- libs/utils/validator.py
+++ libs/utils/validator.py
@@ -65,7 +65,7 @@ def check_namepattern(name: str, kind: Literal["member", "team"]) -> tuple[bool,
     if ret_flg and CommandParser().is_valid_command(name):
         ret_flg, ret_msg = False, "オプションに使用される単語では登録できません。"
 
-    if ret_flg and name in g.cfg.word_list():
+    if ret_flg and name in set(list(g.keyword_dispatcher.keys()) + list(g.command_dispatcher.keys())):
         ret_flg, ret_msg = False, "コマンドに使用される単語では登録できません。"
 
     return (ret_flg, ret_msg)
