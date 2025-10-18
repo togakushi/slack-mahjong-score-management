1bd0cc3e6dcfcbdf5558218f19ead1254bcf910d
togakushi <>
2025-10-18 11:12:22 +0900

    discord連携予定地

---
+++ integrations/discord/__init__.py
@@ -0,0 +1,10 @@
+"""
+discord連携
+
+- `integrations.discord.events`: イベント処理
+- `integrations.discord.adapter`: サービスアダプタ定義
+- `integrations.discord.api`: インターフェースAPI
+- `integrations.discord.config`: 個別設定
+- `integrations.discord.parser`: メッセージ解析クラス
+- `integrations.discord.functions`: 専用関数群
+"""
