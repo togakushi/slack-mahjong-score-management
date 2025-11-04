0533d797f2ed12c56912c8e57a4920c472e689f3
togakushi <>
2025-11-04 20:56:31 +0900

    nullを除外

--- libs/functions/search.py
+++ libs/functions/search.py
@@ -27,7 +27,7 @@ def for_db_score(first_ts: float) -> list[GameResult]:
     with closing(dbutil.connection(g.cfg.setting.database_file)) as conn:
         curs = conn.cursor()
         rows = curs.execute(
-            "select * from result where ts >= ? and (source isnull or source like ?)",
+            "select * from result where ts >= ? and source like ?",
             (str(first_ts), f"{g.adapter.interface_type}_%"))
         for row in rows.fetchall():
             data.append(GameResult(**dict(row)))
