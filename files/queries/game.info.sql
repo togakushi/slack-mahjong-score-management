1e75bb4d0b63fa58718485d608b86c3e47082524
togakushi <>
2025-11-16 02:10:41 +0900

    コメント抽出方法変更

--- files/queries/game.info.sql
+++ files/queries/game.info.sql
@@ -1,20 +1,10 @@
 -- game.info
-select
-    game_count as count,
-    first_game, last_game,
-    --[group_length] substr(first_comment, 1, :group_length) as first_comment,
-    --[group_length] substr(last_comment, 1, :group_length) as last_comment,
-    --[not_group_length] first_comment, last_comment,
-    unique_name, unique_team
-from (
+with game_data as (
     select
-        count(distinct game_info.playtime) as game_count,
-        min(game_info.playtime) as first_game,
-        max(game_info.playtime) as last_game,
-        first_value(game_info.comment) over(order by game_info.playtime asc) as first_comment,
-        last_value(game_info.comment) over(order by game_info.playtime asc) as last_comment,
-        count(distinct individual_results.name) as unique_name,
-        count(distinct individual_results.team) as unique_team
+        game_info.playtime as playtime,
+        game_info.comment as comment,
+        individual_results.name as name,
+        individual_results.team as team
     from
         game_info
     join individual_results on
@@ -27,4 +17,27 @@ from (
         --[search_word] and game_info.comment like :search_word
     order by
         game_info.playtime asc
-);
+)
+select
+    game_count as count,
+    first_game, last_game,
+    --[group_length] substr(first_comment, 1, :group_length) as first_comment,
+    --[group_length] substr(last_comment, 1, :group_length) as last_comment,
+    --[not_group_length] first_comment, last_comment,
+    unique_name, unique_team
+from (
+	select
+		dense_rank() over(order by playtime, name, team) as game_count,
+		first_value(playtime) over(order by playtime asc) as first_game,
+		last_value(playtime) over(order by playtime desc) as last_game,
+		first_value(comment) over(order by playtime asc) as first_comment,
+		last_value(comment) over(order by playtime desc) as last_comment,
+		(select count(distinct name) from game_data) as unique_name,
+		(select count(distinct team) from game_data) as unique_team
+	from
+		game_data
+)
+order by
+	game_count desc
+limit 1
+;
