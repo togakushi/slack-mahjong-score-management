15d8f3696d5dcbb6aca6f1c21f41a69f16fc595f
togakushi <>
2025-11-17 11:38:10 +0900

    カラム名修正

--- libs/utils/converter.py
+++ libs/utils/converter.py
@@ -252,7 +252,7 @@ def df_to_results_simple(df: pd.DataFrame) -> dict:
             vs_guest = f"({g.cfg.setting.guest_mark}) "
 
         ret = f"\t{vs_guest}{str(x["日時"]).replace("-", "/")}  "
-        ret += f"{x["座席"]}\t{x["順位"]}位\t{x["素点"]:8d}点\t({x["ポイント"]:7.1f}pt)\t{x["役満和了"]}".replace("-", "▲")
+        ret += f"{x["座席"]}\t{x["順位"]}位\t{x["素点"]:8d}点\t({x["獲得ポイント"]:7.1f}pt)\t{x["役満和了"]}".replace("-", "▲")
         data_list.append(ret)
 
     return {str(idx): x for idx, x in enumerate(formatter.group_strings(data_list, 2500))}
