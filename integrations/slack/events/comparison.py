beb8db5f7523761490e32289603b6dcbf9289adb
togakushi <>
2025-11-27 11:09:26 +0900

    コマンド判定を修正

--- integrations/slack/events/comparison.py
+++ integrations/slack/events/comparison.py
@@ -63,7 +63,7 @@ def check_omission(results: ComparisonResults):
     keep_channel_id: list = []
 
     for work_m in set(g.adapter.functions.pickup_score()):
-        if work_m.keyword != g.cfg.setting.keyword:
+        if work_m.keyword in g.keyword_dispatcher:  # コマンドキーワードはスキップ
             continue
         if (score := GameResult(**work_m.get_score(g.cfg.setting.keyword), **g.cfg.mahjong.to_dict())):
             for k, v in score.to_dict().items():  # 名前の正規化
