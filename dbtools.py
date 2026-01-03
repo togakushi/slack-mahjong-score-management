17d2a73a925d5c52636b563ba159e6bb0ac85f2b
togakushi <>
2026-01-03 22:39:27 +0900

    Refactor help documentation

--- dbtools.py
+++ dbtools.py
@@ -1,37 +1,34 @@
 #!/usr/bin/env python3
 """
-dbtools.py - 補助ツール
+dbtools.py - DB管理補助ツール
 
 help:
+    usage: dbtools.py [-h] ...
 
-usage: dbtools.py [-h] [-c CONFIG] [--service {slack,standard_io,std,web,flask}]
-                  [--debug] [--verbose] [--moderate] [--notime]
-                  [--compar | --unification [UNIFICATION] | --recalculation | --export [PREFIX] | --import [PREFIX] | --vacuum | --gen-test-data [count]]
+    options:
+        -h, --help            show this help message and exit
+        -c CONFIG, --config CONFIG
+                              設定ファイル(default: config.ini)
+        --service {slack,standard_io,std,web,flask}
+                              連携先サービス
 
-options:
-  -h, --help            show this help message and exit
-  -c CONFIG, --config CONFIG
-                        設定ファイル(default: config.ini)
-  --service {slack,standard_io,std,web,flask}
-                        連携先サービス
+    logging options:
+        -d, --debug           デバッグレベル(-d, -dd)
+        -v, --verbose         動作ログ出力レベル(-v, -vv, -vvv)
+        --moderate            ログレベルがエラー以下のもを非表示
+        --notime              ログフォーマットから日時を削除
 
-logging options:
-  --debug               デバッグ情報表示
-  --verbose, --trace    詳細デバッグ情報表示
-  --moderate            ログレベルがエラー以下のもを非表示
-  --notime              ログフォーマットから日時を削除
-
-Required options(amutually exclusive):
-  --compar              データ突合
-  --unification [UNIFICATION]
-                        ファイルの内容に従って記録済みのメンバー名を修正する(default: rename.ini)
-  --recalculation       ポイント再計算
-  --export [PREFIX]     メンバー設定情報をエクスポート(default prefix: export)
-  --import [PREFIX]     メンバー設定情報をインポート(default prefix: export)
-  --vacuum              database vacuum
-  --gen-test-data [count]
-                        テスト用サンプルデータ生成(count=生成回数, default: 1)
-"""  # noqa: E501
+    Required options(amutually exclusive):
+        --compar              データ突合
+        --unification [UNIFICATION]
+                              ファイルの内容に従って記録済みのメンバー名を修正する(default: rename.ini)
+        --recalculation       ポイント再計算
+        --export [PREFIX]     メンバー設定情報をエクスポート(default prefix: export)
+        --import [PREFIX]     メンバー設定情報をインポート(default prefix: export)
+        --vacuum              database vacuum
+        --gen-test-data [count]
+                              テスト用サンプルデータ生成(count=生成回数, default: 1)
+"""
 
 import libs.global_value as g
 from libs import configuration
