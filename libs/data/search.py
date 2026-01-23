07b82458bf0c12e7bbaecd819357e2182691c52d
togakushi <>
2026-01-24 02:10:08 +0900

    Refactor for_db_score function to use loader for database queries and improve logging

--- libs/data/search.py
+++ libs/data/search.py
@@ -2,11 +2,13 @@
 libs/data/search.py
 """
 
+import logging
 from contextlib import closing
 from typing import TYPE_CHECKING
 
 import libs.global_value as g
 from cls.score import GameResult
+from libs.data import loader
 from libs.utils import dbutil
 
 if TYPE_CHECKING:
@@ -24,15 +26,15 @@ def for_db_score(first_ts: float) -> list[GameResult]:
     """
 
     data: list = []
-    with closing(dbutil.connection(g.cfg.setting.database_file)) as conn:
-        curs = conn.cursor()
-        rows = curs.execute(
-            "select * from result where ts >= ? and source like ?",
-            (str(first_ts), f"{g.adapter.interface_type}_%"),
-        )
-        for row in rows.fetchall():
-            data.append(GameResult(**dict(row)))
+    rows = loader.execute(
+        "select * from result where ts >= :first_ts and source like :source",
+        {"first_ts": str(first_ts), "source": f"{g.adapter.interface_type}_%"},
+    )
 
+    for row in rows:
+        data.append(GameResult(**row))
+
+    logging.debug(data)
     return data
 
 
@@ -63,5 +65,5 @@ def for_db_remarks(first_ts: float) -> list["RemarkDict"]:
                     "source": row["source"],
                 }
             )
-
+    logging.debug(data)
     return data
